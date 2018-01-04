9d0c8b760fb00eae37aeaac9b231504f2d7c6fd4
Elmar Schug <elmar.schug@jayware.org>
2018-01-04 18:06:48 +0100

    Fixed variable definition in Field trait.

--- keyscore-model/src/main/scala/io/logbee/keyscore/model/Field.scala
+++ keyscore-model/src/main/scala/io/logbee/keyscore/model/Field.scala
@@ -1,11 +1,18 @@
 package io.logbee.keyscore.model
 
 trait Field {
-  def name: String
+  val name: String
+  val kind: String
 }
 
-case class TextField(name: String, value: String) extends Field
+case class TextField(name: String, value: String) extends Field {
+  override val kind: String = "text"
+}
 
-case class NumberField(name: String, value: BigDecimal) extends Field
+case class NumberField(name: String, value: BigDecimal) extends Field {
+  override val kind: String = "number"
+}
 
-case class TimestampField(name: String, value: Long) extends Field
+case class TimestampField(name: String, value: Long) extends Field {
+  override val kind: String = "timestamp"
+}
