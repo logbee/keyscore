78894f3632f93df8f46c3eccaba261704300cb11
Elmar Schug <elmar.schug@jayware.org>
2018-01-04 15:37:13 +0100

    Deleted old stuff.

--- keyscore-model/src/main/scala/io/logbee/keyscore/model/StreamBlueprint.scala
+++
@@ -1,7 +0,0 @@
-package io.logbee.keyscore.model
-
-import java.util.UUID
-
-import io.logbee.keyscore.model.filter.FilterBlueprint
-
-case class StreamBlueprint(id: UUID, filters: Seq[FilterBlueprint])
