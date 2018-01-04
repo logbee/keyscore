b33b0c32b9abbc65a5ac1991c9c2afea20bf7a6c
Elmar Schug <elmar.schug@jayware.org>
2018-01-04 10:01:18 +0100

    Implemented an initial approach of a GrokFilter.

---
+++ keyscore-frontier/src/main/scala/io/logbee/keyscore/frontier/filter/GrokFilter.scala
@@ -0,0 +1,126 @@
+package io.logbee.keyscore.frontier.filter
+
+import akka.stream._
+import akka.stream.stage.{GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
+import io.logbee.keyscore.model.{Event, Field, TextField}
+
+import scala.collection.mutable
+import scala.concurrent.{Future, Promise}
+import scala.util.matching.Regex
+
+sealed trait GrokFilterHandle {
+
+  def configure(config: GrokFilterConfiguration): Future[Boolean]
+}
+
+object GrokFilter {
+
+  def apply(): GrokFilter = GrokFilter(GrokFilterConfiguration(
+    isPaused = Option(true)
+  ))
+
+  def apply(config: GrokFilterConfiguration): GrokFilter = new GrokFilter(config)
+}
+
+class GrokFilter(config: GrokFilterConfiguration) extends GraphStageWithMaterializedValue[FlowShape[Event, Event], Future[GrokFilterHandle]] {
+
+  val in: Inlet[Event] = Inlet[Event]("grok.in")
+
+  val out: Outlet[Event] = Outlet[Event]("grok.out")
+
+  override val shape = FlowShape(in, out)
+
+  override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[GrokFilterHandle]) = {
+    val logic = new AwesomeFilterGraphStageLogic(shape, config)
+    (logic, logic.promise.future)
+  }
+
+  private class AwesomeFilterGraphStageLogic(shape: Shape, val initialConfig: GrokFilterConfiguration) extends GraphStageLogic(shape) with InHandler with OutHandler {
+
+    val promise = Promise[GrokFilterHandle]
+
+    private val groupNamePattern: Regex = "\\(\\?<(\\w*)>".r
+    private var isPaused = false
+    private var fieldNames = List.empty[String]
+    private var regex: Regex = "".r
+
+    private val handle = new GrokFilterHandle {
+      private val callback = getAsyncCallback[(GrokFilterConfiguration, Promise[Boolean])] {
+        case (newConfig, promise) =>
+
+          val currentlyPaused = isPaused
+
+          update(newConfig)
+
+          if (currentlyPaused && !isPaused) {
+            if (isAvailable(in)) {
+              push(out, filter(grab(in)))
+            } else if (isAvailable(out) && !hasBeenPulled(in)) {
+              pull(in)
+            }
+          }
+
+          promise.success(true)
+      }
+
+      override def configure(config: GrokFilterConfiguration): Future[Boolean] = {
+        val promise = Promise[Boolean]()
+        callback.invoke((config, promise))
+        promise.future
+      }
+    }
+
+    setHandlers(in, out, this)
+
+    override def preStart(): Unit = {
+      update(initialConfig)
+      promise.success(handle)
+    }
+
+    def filter(event: Event): Event = {
+
+      val payload = new mutable.HashMap[String, Field]
+
+      for (field <- event.payload.values) {
+        payload.put(field.name, field)
+        if (fieldNames.contains(field.name) && field.isInstanceOf[TextField]) {
+          regex.findFirstMatchIn(field.asInstanceOf[TextField].value)
+            .foreach(m => m.groupNames.map(name => TextField(name, m.group(name)))
+              .foreach(textField => payload.put(textField.name, textField)))
+        }
+      }
+
+      Event(event.id, payload.toMap)
+    }
+
+    override def onPush(): Unit = {
+      if (isOpen) {
+        push(out, filter(grab(in)))
+      }
+    }
+
+    override def onPull(): Unit = {
+      if (isOpen) {
+        pull(in)
+      }
+    }
+
+    private def isOpen = !isPaused
+
+    private def update(config: GrokFilterConfiguration): Unit = {
+      isPaused = config.isPaused.getOrElse(isPaused)
+      fieldNames = config.fieldNames.getOrElse(fieldNames)
+      config.pattern match {
+        case Some(pattern) =>
+          regex = pattern.r(groupNamePattern.findAllMatchIn(pattern).map(_.group(1)).toSeq: _*)
+        case None =>
+      }
+    }
+  }
+}
+
+case class GrokFilterConfiguration(
+  isPaused: Option[Boolean] = Some(false),
+  fieldNames: Option[List[String]] = None,
+  pattern: Option[String] = None
+)
---
+++ keyscore-frontier/src/test/scala/io/logbee/keyscore/frontier/app/GrokFilterExample.scala
@@ -0,0 +1,64 @@
+package io.logbee.keyscore.frontier.app
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink}
+import akka.stream.testkit.scaladsl.TestSource
+import io.logbee.keyscore.frontier.filter.{GrokFilter, GrokFilterConfiguration}
+import io.logbee.keyscore.model.{Event, TextField}
+
+import scala.io.StdIn
+import scala.util.Success
+
+object GrokFilterExample extends App {
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = materializer.executionContext
+
+  private val configuration = GrokFilterConfiguration(
+    isPaused = Some(true),
+    fieldNames = Some(List("message")),
+    pattern = Some("=(?<state>\\w*)")
+  )
+
+  val (sourceProbe, switchFut) = TestSource.probe[Event]
+    .viaMat(GrokFilter(configuration))(Keep.both)
+    .map(number => s"> $number")
+    .toMat(Sink.foreach(println))(Keep.left)
+    .run()
+
+  switchFut.onComplete {
+    case Success(switch) =>
+
+      println("ANY KEY [1]")
+      StdIn.readLine()
+
+      sourceProbe.sendNext(Event(TextField("message", "Hello World")))
+      sourceProbe.sendNext(Event(TextField("message", "This is a Test where A=42")))
+      sourceProbe.sendNext(Event(TextField("message", "This is a Test where A=73")))
+
+      println("ANY KEY [2]")
+      StdIn.readLine()
+
+      println("Open Valve")
+      switch.configure(GrokFilterConfiguration(isPaused = Some(false))).onComplete {
+        case Success(success) =>
+          sourceProbe.sendNext(Event())
+          sourceProbe.sendNext(Event())
+          switch.configure(GrokFilterConfiguration(isPaused = Some(true), pattern = Some(":\\s?(?<state>\\w*)"))).onComplete {
+            case Success(success) =>
+              sourceProbe.sendNext(Event(TextField("message", "Hello World foo: fubar")))
+              sourceProbe.sendNext(Event(TextField("message", "Hello World muh: kuh")))
+              println("ANY KEY [3]")
+              StdIn.readLine()
+
+              switch.configure(GrokFilterConfiguration(isPaused = Some(false)))
+
+              println("ANY KEY [4]")
+              StdIn.readLine()
+              system.terminate()
+          }
+      }
+  }
+}
