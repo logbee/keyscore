2e3a79f488c83287d5423bd49d74035559b950f0
Elmar Schug <elmar.schug@jayware.org>
2018-02-20 12:13:59 +0100

    Dataset does now implement a Seq.

--- keyscore-model/src/main/scala/io/logbee/keyscore/model/Dataset.scala
+++ keyscore-model/src/main/scala/io/logbee/keyscore/model/Dataset.scala
@@ -1,5 +1,10 @@
 package io.logbee.keyscore.model
 
-class Dataset(val records: List[Record]) {
+class Dataset(private val records: List[Record]) extends Seq[Record] {
 
+  override def apply(idx: Int): Record = records(idx)
+
+  override def iterator: Iterator[Record] = records.iterator
+
+  override def length: Int = records.length
 }
