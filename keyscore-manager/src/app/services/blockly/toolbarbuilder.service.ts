0a9e9503e839104665293a330ce66c13d3fe2cec
Max Karthan <ich@max-karthan.de>
2018-04-30 17:39:12 +0200

    added parameters and color to blockly + Observable for currently selected block

--- keyscore-manager/src/app/services/blockly/toolbarbuilder.service.ts
+++ keyscore-manager/src/app/services/blockly/toolbarbuilder.service.ts
@@ -2,27 +2,27 @@ import {Injectable} from "@angular/core";
 import {FilterDescriptor} from "../../streams/streams.model";
 import {Blockly} from "node-blockly/browser";
 
-declare var Blockly:any;
+declare var Blockly: any;
 
 @Injectable()
 export class ToolBarBuilderService {
     constructor() {
     }
 
-    createToolbar(filterDescriptors:FilterDescriptor[],categories:string[]):string{
+    createToolbar(filterDescriptors: FilterDescriptor[], categories: string[]): string {
         let parser = new DOMParser();
         let serializer = new XMLSerializer();
         let xml = '<xml xmlns="http://www.w3.org/1999/xhtml" id="toolbox" style="display: none;"></xml>';
-        let xmlDoc = parser.parseFromString(xml,"text/xml");
+        let xmlDoc = parser.parseFromString(xml, "text/xml");
 
-        categories.forEach(cat =>{
+        categories.forEach(cat => {
             let currentXmlCategory = xmlDoc.createElement("category");
-            currentXmlCategory.setAttribute("name",cat);
-            filterDescriptors.forEach(descriptor =>{
-                if(descriptor.category === cat){
+            currentXmlCategory.setAttribute("name", cat);
+            filterDescriptors.forEach(descriptor => {
+                if (descriptor.category === cat) {
                     this.createBlock(descriptor);
                     let currentXmlBlock = xmlDoc.createElement("block");
-                    currentXmlBlock.setAttribute("type",descriptor.name);
+                    currentXmlBlock.setAttribute("type", descriptor.name);
                     currentXmlCategory.appendChild(currentXmlBlock);
                 }
             })
@@ -30,24 +30,31 @@ export class ToolBarBuilderService {
         })
 
         let xmlString = serializer.serializeToString(xmlDoc);
-        console.log("toolbox: "+xmlString);
+        console.log("toolbox: " + xmlString);
         return xmlString;
     }
 
-    private createBlock(descriptor:FilterDescriptor){
+    private createBlock(descriptor: FilterDescriptor) {
         Blockly.Blocks[descriptor.name] = {
-            init: function(){
+            init: function () {
                 this.setPreviousStatement(descriptor.previousConnection.isPermitted);
                 this.setNextStatement(descriptor.nextConnection.isPermitted);
+                this.setColour(descriptor.nextConnection.isPermitted && descriptor.previousConnection.isPermitted ? 90 :
+                descriptor.nextConnection.isPermitted && !descriptor.previousConnection.isPermitted ? 180 : 360);
                 this.appendDummyInput()
                     .appendField(descriptor.displayName);
+                this.setTooltip(descriptor.description);
                 descriptor.parameters.forEach(p => {
-                    switch (p.kind){
-                        case 'text': this.appendDummyInput().appendField(p.displayName).appendField(new Blockly.FieldTextInput(''),p.name);
-                        break;
-                        case 'int':this.appendDummyInput().appendField(p.displayName).appendField(new Blockly.FieldNumber('0'),p.name);
-                        break;
-                        case 'boolean': this.append
+                    switch (p.kind) {
+                        case 'text':
+                            this.appendDummyInput().appendField(p.displayName).appendField(new Blockly.FieldTextInput(p.displayName), p.name);
+                            break;
+                        case 'int':
+                            this.appendDummyInput().appendField(p.displayName).appendField(new Blockly.FieldNumber('0'), p.name);
+                            break;
+                        case 'boolean':
+                            this.appendDummyInput().appendField(p.displayName).appendField(new Blockly.FieldCheckbox('FALSE'), p.name);
+                            break;
                     }
                 })
             }
--- keyscore-manager/src/app/streams/stream-editor/blockly/blockly.component.ts
+++ keyscore-manager/src/app/streams/stream-editor/blockly/blockly.component.ts
@@ -6,6 +6,7 @@ import {FilterDescriptor, getFilterCategories, getFilterDescriptors, StreamsStat
 import {Observable} from "rxjs/Observable";
 import {ToolBarBuilderService} from "../../../services/blockly/toolbarbuilder.service";
 import {LoadFilterDescriptorsAction} from "../../streams.actions";
+import {ReplaySubject} from "rxjs/ReplaySubject";
 
 
 declare var Blockly: any;
@@ -14,12 +15,15 @@ declare var Blockly: any;
     selector: 'blockly-workspace',
     template: `
         <div class="row" style="min-height:600px">
-            <div id="blocklyArea" class="col-9" style="position:relative" >
+            <div id="blocklyArea" class="col-9" style="position:relative">
                 <div id="blocklyDiv" style="position: absolute"></div>
             </div>
-            <textarea id="code" class="col-2 ml-3" [(ngModel)]="generatedCode"></textarea>
+            <div id="code" class="col-2 ml-3">
+                <div class="col-12" style="font-size: 22px">{{(currentBlock$ | async).name}}</div>
+                <div class="col-12">{{(currentBlock$ | async).description}}</div>
+            </div>
         </div>
-        
+
     `,
 
     providers: [
@@ -33,16 +37,21 @@ export class BlocklyComponent implements OnInit {
     private filterDescriptors$: Observable<FilterDescriptor[]>;
     private toolbox: any;
 
+    private currentXml: XMLDocument;
     private blocklyDiv;
     private blocklyArea;
 
-    generatedCode = "Here goes the code";
+    private selectedBlock$: ReplaySubject<String> = new ReplaySubject();
+    private currentBlock$: ReplaySubject<{ name: string, description: string }> = new ReplaySubject();
 
     constructor(private store: Store<StreamsState>, private toolbarBuilder: ToolBarBuilderService) {
         this.filterDescriptors$ = this.store.select(getFilterDescriptors);
         this.categories$ = this.store.select(getFilterCategories);
+        this.currentBlock$.next({name:'Test',description:'Beispiel Beschreibung'});
+        this.selectedBlock$.next('');
 
-        this.store.dispatch(new LoadFilterDescriptorsAction());
+        this.store.dispatch(new LoadFilterDescriptorsAction())
+
     }
 
     ngOnInit(): void {
@@ -59,6 +68,10 @@ export class BlocklyComponent implements OnInit {
             this._workspace.addChangeListener((e: any) => this.onWorkspaceChange(e));
         });
 
+        this.selectedBlock$.combineLatest(this.filterDescriptors$).subscribe(([blockName,descriptors]) =>{
+            descriptors.filter(descriptor => descriptor.name === blockName).forEach(d => this.currentBlock$.next({name:d.displayName,description:d.description}));
+        })
+
         window.addEventListener('resize', this.onResize, false);
         this.onResize(null);
         Blockly.svgResize(this._workspace);
@@ -66,7 +79,11 @@ export class BlocklyComponent implements OnInit {
 
     onWorkspaceChange(e: any) {
         console.log(e);
-        this.generatedCode = Blockly.JavaScript.workspaceToCode(this._workspace);
+        this.currentXml = Blockly.Xml.workspaceToDom(this._workspace);
+        console.log(this.currentXml);
+        if(e.element === 'selected') {
+            this.updateSelectedBlock(e.newValue);
+        }
     }
 
     onResize(e: any) {
@@ -77,6 +94,16 @@ export class BlocklyComponent implements OnInit {
         this.blocklyDiv.style.top = element.offsetTop + 'px';
         this.blocklyDiv.style.width = this.blocklyArea.offsetWidth + 'px';
         this.blocklyDiv.style.height = this.blocklyArea.offsetHeight + 'px';
+
+    }
+    private updateSelectedBlock(id: string) {
+        let allBlocks = this.currentXml.getElementsByTagName("block");
+        for(let i=0;i<allBlocks.length;i++){
+            if(allBlocks[i].getAttribute('id') === id){
+                this.selectedBlock$.next(allBlocks[i].getAttribute('type'));
+                break;
+            }
+        }
 
     }
 }
