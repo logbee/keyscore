e6aaf4587368606c5b41d70958116760036cb9ce
mlandth <max.landthaler@posteo.de>
2018-05-04 13:08:37 +0200

    Approach to implement a CSV filter with configurable separator.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/stream/contrib/CSVParserFilterFunction.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/stream/contrib/CSVParserFilterFunction.scala
@@ -1,17 +1,46 @@
 package io.logbee.keyscore.agent.stream.contrib
 
-import io.logbee.keyscore.model.{Dataset, Described}
+import io.logbee.keyscore.model._
 import io.logbee.keyscore.model.filter._
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object CSVParserFilterFunction extends Described {
   override def descriptor: FilterDescriptor = FilterDescriptor("CSVFilter", "Filter that parses csv in a readable format with key and value.", List(
     ListParameterDescriptor("headers", TextParameterDescriptor("headerName"), min = 1),
-    TextParameterDescriptor("record")
+    TextParameterDescriptor("separator")
   ))
 }
 
 class CSVParserFilterFunction extends FilterFunction {
-  override def configure(configuration: FilterConfiguration): Unit = ???
+  var headerList : List[String] = List[String]()
+  var separator : String = ""
 
-  override def apply(dataset: Dataset): Dataset = ???
+  override def configure(configuration: FilterConfiguration): Unit = {
+    for (parameter <- configuration.parameters)
+      parameter.name match {
+        case "separator" =>
+          separator = parameter.value.asInstanceOf[String]
+        case "headers" =>
+          headerList = parameter.value.asInstanceOf[List[String]]
+        case _ =>
+      }
+
+  }
+
+  override def apply(dataset: Dataset): Dataset = {
+    var recordsList =  ListBuffer[Record]()
+    for (record <- dataset){
+      for (field <- record.payload.values) {
+        val message = field.asInstanceOf[TextField].value
+        val listOfData = message.split(separator).map( x => TextField(x,x)).toList
+        val dataMap : Map[String, TextField] = headerList.zip(listOfData).toMap
+        recordsList += new Record(record.id, dataMap)
+      }
+    }
+
+    val dataList = recordsList.toList
+    new Dataset(dataList)
+  }
 }
