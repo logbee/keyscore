63702c9d483f0c6f395586e001286190c4e4e6be
Max Karthan <ich@max-karthan.de>
2018-06-18 17:28:44 +0200

    added gradle tasks to create kafka topics in docker network + some adjustments to frontier and agent docker container for communication in the network

---
+++ test/src/test/scala/io/logbee/keyscore/test/IntegrationTest.scala
@@ -0,0 +1,10 @@
+package io.logbee.keyscore.test
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class IntegrationTest {
+  println("Test is running")
+  assert(true)
+}
