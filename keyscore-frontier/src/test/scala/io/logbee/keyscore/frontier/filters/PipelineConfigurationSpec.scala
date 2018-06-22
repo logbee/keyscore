fd957ab59a99a504bed4fcae849fe31676d11c6b
Max K <ich@max-karthan.de>
2018-06-22 21:52:38 +0200

    fixed reading the example pipeline configuration in PipelineIntegrationTest

--- keyscore-frontier/src/test/scala/io/logbee/keyscore/frontier/filters/PipelineConfigurationSpec.scala
+++ keyscore-frontier/src/test/scala/io/logbee/keyscore/frontier/filters/PipelineConfigurationSpec.scala
@@ -4,14 +4,12 @@ import java.io.InputStreamReader
 
 import io.logbee.keyscore.frontier.json.helper.FilterConfigTypeHints
 import io.logbee.keyscore.model.PipelineConfiguration
-import io.logbee.keyscore.model.filter.FilterConfiguration
-import org.scalatest.{Matchers, WordSpecLike}
-import org.json4s.NoTypeHints
 import org.json4s.ext.JavaTypesSerializers
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization.read
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpecLike}
 
 @RunWith(classOf[JUnitRunner])
 class PipelineConfigurationSpec extends WordSpecLike with Matchers {
--- test/src/test/scala/io/logbee/keyscore/test/PipelineIntegrationTest.scala
+++ test/src/test/scala/io/logbee/keyscore/test/PipelineIntegrationTest.scala
@@ -16,6 +16,8 @@ import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization.write
+
+import scala.io.Source
 @ExtendWith(value = Array(classOf[CitrusExtension]))
 class PipelineIntegrationTest extends Matchers {
   private val httpClient: HttpClient = CitrusEndpoints.http()
@@ -27,14 +29,12 @@ class PipelineIntegrationTest extends Matchers {
   @Test
   @CitrusTest
   def createPipeline(@CitrusResource runner: TestRunner): Unit = {
-    implicit val formats = Serialization.formats(NoTypeHints) ++
-      org.json4s.ext.JavaTypesSerializers.all
-    print(write(ExampleData.pipelineConfiguration))
+    val pipeLineConfig = Source.fromResource("pipelineConfiguration.example.json").mkString
     runner.http(action => action.client(httpClient)
       .send()
       .put("/pipeline/configuration")
       .contentType("application/json")
-      .payload(write(ExampleData.pipelineConfiguration))
+      .payload(pipeLineConfig)
     )
 
     runner.http(action => action.client(httpClient)
