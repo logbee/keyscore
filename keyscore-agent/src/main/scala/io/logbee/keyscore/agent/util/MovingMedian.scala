0f7b3f801f1f2a334f79c47799580a9e154cc081
mlandth <max.landthaler@posteo.de>
2018-06-29 16:55:59 +0200

    Implemented the + and get function for MovingMedian.scala to issue #13 +tbc.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/util/MovingMedian.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/util/MovingMedian.scala
@@ -1,25 +1,59 @@
 package io.logbee.keyscore.agent.util
 
+import io.logbee.keyscore.agent.util.MovingMedian.MovingMedianItem
+
 
 object MovingMedian {
+
   def apply(window: Int = 10): MovingMedian = new MovingMedian(window)
 
   implicit def median2Long(median: MovingMedian): Long = {
     median.get
   }
+
+  case class MovingMedianItem(throughputTime: Long, currentSystemTime: Long = System.currentTimeMillis()) extends Ordered[MovingMedianItem] {
+    override def compare(that: MovingMedianItem): Int = {
+      this.currentSystemTime compare that.currentSystemTime
+    }
+  }
 }
 
 class MovingMedian(window: Int) {
 
+  var medians: Array[MovingMedianItem] = Array.ofDim(window)
+
   def +(value: Long): MovingMedian = {
+    val item = MovingMedianItem(value)
+    var indexToUpdate = 0
+    val itemsCount = numberOfItems
+    if (itemsCount < window) {
+      indexToUpdate = itemsCount
+    } else {
+      indexToUpdate = oldestIndex
+    }
+    medians.update(indexToUpdate, item)
     this
   }
 
   def get: Long = {
-      0
+    var time: Long = Long.MinValue
+    if (medians.nonEmpty) {
+      val medianItem = medians(numberOfItems / 2)
+      time = medianItem.throughputTime
+    }
+    time
   }
 
   def reset(): Unit = {
-
+    medians = Array.empty
   }
+
+  private def numberOfItems: Int = {
+    medians.foldLeft(0)((number, item) => if (item.isInstanceOf[MovingMedianItem]) number + 1 else number)
+  }
+
+  private def oldestIndex: Int = {
+    medians.indexOf(medians.min)
+  }
+
 }
--- keyscore-agent/src/test/scala/io/logbee/keyscore/agent/util/MovingMedianSpec.scala
+++ keyscore-agent/src/test/scala/io/logbee/keyscore/agent/util/MovingMedianSpec.scala
@@ -1,5 +1,6 @@
 package io.logbee.keyscore.agent.util
 
+import io.logbee.keyscore.agent.util.MovingMedian.MovingMedianItem
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
@@ -10,6 +11,36 @@ class MovingMedianSpec extends WordSpec with Matchers {
   "An initialized MovingMedian" should {
     "return 0" in {
       MovingMedian().get shouldBe 0
+    }
+  }
+
+  private val min: Int = 1
+  private val max: Int = 42
+  private val initMedians = Array(MovingMedianItem(42, min + 3), MovingMedianItem(37, min + 5), MovingMedianItem(12, max), MovingMedianItem(31, min), MovingMedianItem(75, min + 7))
+  private var movingMedian = MovingMedian(10)
+
+  "An Array of MovingMedianItems" should {
+    "return the minimal item" in {
+      val minMedian = initMedians.min
+      val oldestTime = minMedian.currentSystemTime
+      oldestTime shouldBe (min)
+    }
+    "return the right index of a minimal item" in {
+      initMedians.indexOf(initMedians.min) shouldBe (3)
+    }
+    "update a new item correct" in {
+      movingMedian + 10
+      movingMedian + 11
+      movingMedian + 12
+      movingMedian + 13
+      movingMedian + 14
+      movingMedian + 15
+      movingMedian + 16
+      movingMedian + 17
+      movingMedian + 18
+      movingMedian + 19
+      Thread.sleep(1000)
+      movingMedian.get shouldBe (15)
     }
   }
 }
