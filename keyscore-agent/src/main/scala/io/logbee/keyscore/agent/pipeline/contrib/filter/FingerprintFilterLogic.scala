ab4f89c2a4519b56cd8fbd45cb52a9b5cea4f116
Elmar Schug <elmar.schug@jayware.org>
2018-08-06 11:09:31 +0200

    Enhanced FingerprintFilter, made base64 encoding optional.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/FingerprintFilterLogic.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/FingerprintFilterLogic.scala
@@ -9,8 +9,9 @@ import akka.stream.stage.StageLogging
 import com.google.common.base.Charsets
 import com.google.common.io.BaseEncoding.base64
 import io.logbee.keyscore.agent.pipeline.stage.{FilterLogic, StageContext}
+import io.logbee.keyscore.model.Described
 import io.logbee.keyscore.model.filter._
-import io.logbee.keyscore.model.{Dataset, Described, Record, TextField}
+import io.logbee.keyscore.model._
 
 import scala.collection.mutable
 
@@ -37,38 +38,48 @@ object FingerprintFilterLogic extends Described {
       description = translatedText.getString("description"),
       previousConnection = FilterConnection(isPermitted = true),
       nextConnection = FilterConnection(isPermitted = true),
-      category = "Debug",
       parameters = List(
-        TextParameterDescriptor("fingerprintFieldName", displayName = translatedText.getString("fingerprintFieldName"), description = "", mandatory = true, validator = ".*")
+        TextParameterDescriptor("target", displayName = translatedText.getString("target"), description = translatedText.getString("targetDescription"), mandatory = false, validator = ".*"),
+        BooleanParameterDescriptor("base64Encoding", displayName = translatedText.getString("base64Encoding"), description = translatedText.getString("base64EncodingDescription"), mandatory = false)
       ))
   }
 }
 
 class FingerprintFilterLogic(context: StageContext, configuration: FilterConfiguration, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(context, configuration, shape) with StageLogging {
 
-  private var fingerprintFieldName = "fingerprint"
+  private var targetFieldName = "fingerprint"
+  private var base64Encoding = false
+
+  private val digest = MessageDigest.getInstance("MD5")
 
   override def configure(configuration: FilterConfiguration): Unit = {
     configuration.parameters.foreach {
-      case TextParameter("fingerprintFieldName", value) => fingerprintFieldName = value
+      case TextParameter("target", value) if value != null && value.nonEmpty => targetFieldName = value
+      case BooleanParameter("base64Encoding", value) => base64Encoding = value
       case _ =>
     }
   }
 
   override def onPush(): Unit = {
 
-    val digest = MessageDigest.getInstance("MD5")
-
     val dataset = grab(in)
     val records = dataset.records.map(record => {
 
-      val base64MD5Hash = base64().encode(record.payload.values.foldLeft(digest) {
+      var fingerprint: String = "<unknown>"
+      val hashBytes = record.payload.values.foldLeft(digest) {
         case (md5, field) =>
           md5.update(s"${field.name}=${field.value}".getBytes(Charsets.UTF_8))
           md5
-      }.digest())
+      }.digest()
 
-      Record(record.id, record.payload + (fingerprintFieldName -> TextField(fingerprintFieldName, base64MD5Hash)))
+      if (base64Encoding) {
+        fingerprint = base64().encode(hashBytes)
+      }
+      else {
+        fingerprint = hashBytes.map("%02x".format(_)).mkString
+      }
+
+      Record(record.id, record.payload + (targetFieldName -> TextField(targetFieldName, fingerprint)))
     })
 
     push(out, Dataset(dataset.metaData, records))
