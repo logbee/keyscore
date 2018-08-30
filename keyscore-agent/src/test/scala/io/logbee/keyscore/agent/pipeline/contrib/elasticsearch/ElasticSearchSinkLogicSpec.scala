9bf1b8be06472bcc15b1d2f7a16ed3b0c9f5f461
mlandth <max.landthaler@posteo.de>
2018-08-30 16:15:46 +0200

    Migrated ElasticSearchSinkLogicSpec.scala.

--- keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/contrib/elasticsearch/ElasticSearchSinkLogicSpec.scala
+++ keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/contrib/elasticsearch/ElasticSearchSinkLogicSpec.scala
@@ -1,14 +1,14 @@
 package io.logbee.keyscore.agent.pipeline.contrib.elasticsearch
 
-import java.util.UUID.randomUUID
+import java.util.UUID
 
 import akka.stream.SinkShape
 import akka.stream.scaladsl.Keep
 import akka.stream.testkit.scaladsl.TestSource
 import io.logbee.keyscore.agent.pipeline._
-import io.logbee.keyscore.agent.pipeline.stage.{SinkStage, StageContext}
-import io.logbee.keyscore.model.Dataset
-import io.logbee.keyscore.model.filter.{FilterConfiguration, FilterDescriptor, IntParameter, TextParameter}
+import io.logbee.keyscore.agent.pipeline.stage.{LogicParameters, SinkStage, StageContext}
+import io.logbee.keyscore.model.configuration.{Configuration, NumberParameter, TextParameter}
+import io.logbee.keyscore.model.data.Dataset
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
@@ -27,9 +27,17 @@ class ElasticSearchSinkLogicSpec  extends WordSpec with Matchers with ScalaFutur
 
     "do some thing" in {
 
-      val configuration = FilterConfiguration(randomUUID(), FilterDescriptor(randomUUID(), "test"), List(TextParameter("host", "localhost"), IntParameter("port", 9200), TextParameter("index", "test")))
+      val configuration = Configuration(
+        parameters = Seq(
+          TextParameter("host","localhost"),
+          NumberParameter("port", 9200),
+          TextParameter("index","test")
+        )
+      )
+
       val context = StageContext(system, executionContext)
-      val provider = (ctx: StageContext, c: FilterConfiguration, s: SinkShape[Dataset]) => new ElasticSearchSinkLogic(ctx, c, s)
+      val provider = (ctx: StageContext, c: Configuration, s: SinkShape[Dataset]) =>
+        new ElasticSearchSinkLogic(LogicParameters(UUID.randomUUID(), ctx, c), s)
       val elasticSink = new SinkStage(context, configuration, provider)
 
       val (src, sinkFuture) = TestSource.probe[Dataset]
