c10a0ce541baa20135e08393830d6274224a9231
Elmar Schug <elmar.schug@jayware.org>
2018-08-30 14:06:41 +0200

    Updated the KafkaSinkLogic to the new descriptor api. #5

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/kafka/KafkaSinkLogic.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/kafka/KafkaSinkLogic.scala
@@ -1,15 +1,21 @@
 package io.logbee.keyscore.agent.pipeline.contrib.kafka
 
-import java.util.{Locale, ResourceBundle, UUID}
+import java.util.Locale
 
 import akka.kafka.scaladsl.Producer
 import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
 import akka.stream.{OverflowStrategy, SinkShape}
 import com.google.protobuf.util.Timestamps
+import io.logbee.keyscore.agent.pipeline.contrib.kafka.KafkaSinkLogic.{bootstrapServerParameter, bootstrapServerPortParameter, topicParameter}
 import io.logbee.keyscore.agent.pipeline.stage.{SinkLogic, StageContext}
+import io.logbee.keyscore.model.ToOption.T2OptionT
 import io.logbee.keyscore.model._
-import io.logbee.keyscore.model.filter._
+import io.logbee.keyscore.model.configuration.Configuration
+import io.logbee.keyscore.model.data._
+import io.logbee.keyscore.model.descriptor.ExpressionType.RegEx
+import io.logbee.keyscore.model.descriptor._
+import io.logbee.keyscore.model.localization.{Localization, TextRef}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 import org.json4s.ext.JavaTypesSerializers
@@ -17,70 +23,83 @@ import org.json4s.native.Serialization
 import org.json4s.native.Serialization.write
 import org.json4s.{Formats, NoTypeHints}
 
-import scala.collection.mutable
 import scala.concurrent.Promise
 import scala.util.Success
 
 object KafkaSinkLogic extends Described {
 
-  private val filterName = "io.logbee.keyscore.agent.pipeline.contrib.kafka.KafkaSinkLogic"
-  private val bundleName = "io.logbee.keyscore.agent.pipeline.contrib.filter.KafkaSinkLogic"
-  private val filterId = "4fedbe8e-115e-4408-ba53-5b627b6e2eaf"
+  private[kafka] val bootstrapServerParameter = TextParameterDescriptor(
+    ref = "kafka.sink.bootstrapServer",
+    info = ParameterInfo(TextRef("bootstrapServer"), TextRef("bootstrapServerDescription")),
+    defaultValue = "example.com",
+    validator = StringValidator(
+      expression = """^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$""",
+      expressionType = RegEx
+    ),
+    mandatory = true
+  )
 
-  val descriptorMap = mutable.Map.empty[Locale, FilterDescriptorFragment]
+  private[kafka] val bootstrapServerPortParameter = NumberParameterDescriptor(
+    ref = "kafka.sink.bootstrapServerPort",
+    info = ParameterInfo(TextRef("bootstrapServerPort"), TextRef("bootstrapServerPortDescription")),
+    defaultValue = 9092,
+    range = NumberRange(step = 1, end = 65535),
+    mandatory = true
+  )
 
+  private[kafka] val topicParameter = TextParameterDescriptor(
+    ref = "kafka.sink.topic",
+    info = ParameterInfo(TextRef("topic"), TextRef("topicDescription")),
+    defaultValue = "topic",
+    mandatory = true
+  )
 
-  override def describe: MetaFilterDescriptor = {
-    val fragments = Map(
-      Locale.ENGLISH -> descriptor(Locale.ENGLISH),
-      Locale.GERMAN -> descriptor(Locale.GERMAN)
+  override def describe = Descriptor(
+    uuid = "4fedbe8e-115e-4408-ba53-5b627b6e2eaf",
+    describes = SinkDescriptor(
+      name = classOf[KafkaSinkLogic].getName,
+      displayName = TextRef("displayName"),
+      description = TextRef("description"),
+      categories = Seq(TextRef("category1"), TextRef("category2")),
+      parameters = Seq(bootstrapServerParameter, bootstrapServerPortParameter, topicParameter)
+    ),
+    localization = Localization.fromResourceBundle(
+      bundleName = "io.logbee.keyscore.agent.pipeline.contrib.filter.KafkaSinkLogic",
+      Locale.ENGLISH, Locale.GERMAN
     )
-
-    MetaFilterDescriptor(UUID.fromString(filterId), filterName, fragments)
-  }
-
-  private def descriptor(language: Locale) = {
-    val translatedText: ResourceBundle = ResourceBundle.getBundle(bundleName, language)
-    FilterDescriptorFragment(
-      displayName = translatedText.getString("displayName"),
-      description = translatedText.getString("description"),
-      previousConnection = FilterConnection(isPermitted = true),
-      nextConnection = FilterConnection(isPermitted = false),
-      parameters = List(
-            TextParameterDescriptor("bootstrapServer", translatedText.getString("bootstrapServer"), "description"),
-            TextParameterDescriptor("topic", translatedText.getString("topic"), "description")
-      ),
-      translatedText.getString("category"))
-  }
-
+  )
 }
 
-class KafkaSinkLogic(context: StageContext, configuration: FilterConfiguration, shape: SinkShape[Dataset]) extends SinkLogic(context, configuration, shape) {
+class KafkaSinkLogic(context: StageContext, configuration: Configuration, shape: SinkShape[Dataset]) extends SinkLogic(context, configuration, shape) {
 
-  implicit val formats: Formats = Serialization.formats(NoTypeHints) ++ JavaTypesSerializers.all
+  private implicit val formats: Formats = Serialization.formats(NoTypeHints) ++ JavaTypesSerializers.all
   private var queue: SourceQueueWithComplete[ProducerMessage.Message[Array[Byte], String, Promise[Unit]]] = _
 
   private val pullAsync = getAsyncCallback[Unit](_ => {
     pull(in)
   })
 
-  private var topic: String = _
+  private var bootstrapServer = bootstrapServerParameter.defaultValue
+  private var bootstrapServerPort = bootstrapServerPortParameter.defaultValue
+  private var topic = topicParameter.defaultValue
 
   override def postStop(): Unit = {
     log.info("Kafka sink is stopping.")
     queue.complete()
   }
 
-  override def initialize(configuration: FilterConfiguration): Unit = {
+  override def initialize(configuration: Configuration): Unit = {
     configure(configuration)
     pull(in)
   }
 
-  override def configure(configuration: FilterConfiguration): Unit = {
-    val bootstrapServer: String = configuration.getParameterValue[String]("bootstrapServer")
-    topic = configuration.getParameterValue[String]("topic")
+  override def configure(configuration: Configuration): Unit = {
 
-    val settings = producerSettings(bootstrapServer)
+    bootstrapServer = configuration.getValueOrDefault(bootstrapServerParameter, bootstrapServer)
+    bootstrapServerPort = configuration.getValueOrDefault(bootstrapServerPortParameter, bootstrapServerPort)
+    topic = configuration.getValueOrDefault(topicParameter, topic)
+
+    val settings = producerSettings(s"$bootstrapServer:$bootstrapServerPort")
 
     val producer = Producer.flow[Array[Byte], String, Promise[Unit]](settings)
 
@@ -127,5 +146,4 @@ class KafkaSinkLogic(context: StageContext, configuration: FilterConfiguration, 
   }
 
   case class SinkEntry(dataset: Dataset, promise: Promise[Unit])
-
 }
