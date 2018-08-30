c24b4ebd8522e65fc4628db2fb4934e76b8eb09f
mlandth <max.landthaler@posteo.de>
2018-08-30 11:29:37 +0200

    Migrated LoggerFilter.scala to the new api.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/LoggerFilter.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/LoggerFilter.scala
@@ -5,29 +5,36 @@ import java.util.{Locale, UUID}
 import akka.stream.FlowShape
 import akka.stream.stage.StageLogging
 import io.logbee.keyscore.agent.pipeline.stage.{FilterLogic, StageContext}
-import io.logbee.keyscore.model.filter.{FilterConfiguration, FilterConnection, FilterDescriptorFragment, MetaFilterDescriptor}
-import io.logbee.keyscore.model.{Dataset, Described}
+import io.logbee.keyscore.model.Described
+import io.logbee.keyscore.model.ToOption.T2OptionT
+import io.logbee.keyscore.model.configuration.Configuration
+import io.logbee.keyscore.model.data.Dataset
+import io.logbee.keyscore.model.descriptor.{Descriptor, FilterDescriptor}
+import io.logbee.keyscore.model.localization.{Localization, TextRef}
 
 object LoggerFilter extends Described {
 
-  val filterName = "io.logbee.keyscore.agent.pipeline.contrib.filter.LoggerFilter"
   val filterId: UUID = UUID.fromString("9a6f5fd0-a21b-4056-7a2a-344e3b4e2488")
 
-  override def describe: MetaFilterDescriptor = {
-    MetaFilterDescriptor(filterId, filterName, Map(
-      Locale.ENGLISH -> FilterDescriptorFragment(
-        displayName = "Logger Filter",
-        description = "Logs every dataset which passes through.",
-        previousConnection = FilterConnection(isPermitted = true),
-        nextConnection = FilterConnection(isPermitted = true),
-        category = "Debug"
-    )))
-  }
+  override def describe = Descriptor(
+    uuid = "io.logbee.keyscore.agent.pipeline.contrib.filter.LoggerFilter",
+    describes = FilterDescriptor(
+      name = classOf[LoggerFilter].getName,
+      displayName = TextRef("displayName"),
+      description = TextRef("description"),
+      categories = Seq(TextRef("category")),
+      parameters = Seq()
+    ),
+    localization = Localization.fromResourceBundle(
+      bundleName = "io.logbee.keyscore.agent.pipeline.contrib.filter.LoggerFilter",
+      Locale.ENGLISH, Locale.GERMAN
+    )
+  )
 }
 
-class LoggerFilter(context: StageContext, configuration: FilterConfiguration, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(context, configuration, shape) with StageLogging {
+class LoggerFilter(context: StageContext, configuration: Configuration, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(context, configuration, shape) with StageLogging {
 
-  override def configure(configuration: FilterConfiguration): Unit = {
+  override def configure(configuration: Configuration): Unit = {
   }
 
   override def onPush(): Unit = {
