fab6ab71255813b07dccd24cb58820a12a5d0dce
mlandth <max.landthaler@posteo.de>
2018-08-30 10:27:28 +0200

    Migrated GrokFilterLogic.scala to the new api.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/GrokFilterLogic.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/pipeline/contrib/filter/GrokFilterLogic.scala
@@ -1,60 +1,78 @@
 package io.logbee.keyscore.agent.pipeline.contrib.filter
 
-import java.util.{Locale, ResourceBundle, UUID}
+import java.util.Locale
 
 import akka.stream.FlowShape
+import io.logbee.keyscore.agent.pipeline.contrib.filter.GrokFilterLogic.fieldNamesParameter
 import io.logbee.keyscore.agent.pipeline.stage.{FilterLogic, StageContext}
-import io.logbee.keyscore.model._
-import io.logbee.keyscore.model.filter._
+import io.logbee.keyscore.model.Described
+import io.logbee.keyscore.model.ToOption.T2OptionT
+import io.logbee.keyscore.model.configuration.Configuration
+import io.logbee.keyscore.model.data.{Dataset, _}
+import io.logbee.keyscore.model.descriptor.FieldNameHint.PresentField
+import io.logbee.keyscore.model.descriptor._
+import io.logbee.keyscore.model.localization.{Localization, TextRef}
 
+import scala.Int.MaxValue
 import scala.collection.mutable
 import scala.util.matching.Regex
 
-
 object GrokFilterLogic extends Described {
 
-  private val filterName = "io.logbee.keyscore.agent.pipeline.contrib.filter.GrokFilterLogic"
-  private val bundleName = "io.logbee.keyscore.agent.pipeline.contrib.filter.GrokFilter"
-  private val filterId = "8912a691-e982-4680-8fc7-fea6803fcef0"
+  private val fieldNamesParameter = FieldNameListParameterDescriptor(
+    ref = "grok.fieldNames",
+    info = ParameterInfo(
+      displayName = TextRef("fieldNames"),
+      description = TextRef("fieldNamesDescription")
+    ),
+    descriptor = FieldNameParameterDescriptor(
+      hint = PresentField
+    ),
+    min = 1,
+    max = MaxValue
+  )
 
-  override def describe: MetaFilterDescriptor = {
-    val fragments = Map(
-      Locale.ENGLISH -> descriptor(Locale.ENGLISH),
-      Locale.GERMAN -> descriptor(Locale.GERMAN)
+  private val patternParameter = ExpressionParameterDescriptor(
+    ref = "grok.pattern",
+    info = ParameterInfo(
+      displayName = TextRef("patternKeyNameHeader"),
+      description = TextRef("patternKeyDescriptionHeader")
+    ),
+    expressionType = ExpressionType.Grok,
+    mandatory = true
+  )
+
+  override def describe = Descriptor(
+    uuid = "8912a691-e982-4680-8fc7-fea6803fcef0",
+    describes = FilterDescriptor(
+      name = classOf[GrokFilterLogic].getName,
+      displayName = TextRef("displayName"),
+      description = TextRef("description"),
+      categories = Seq(TextRef("category")),
+      parameters = Seq(fieldNamesParameter, patternParameter)
+    ),
+    localization = Localization.fromResourceBundle(
+      "io.logbee.keyscore.agent.pipeline.contrib.filter.GrokFilter",
+      Locale.ENGLISH, Locale.GERMAN
     )
+  )
 
-    MetaFilterDescriptor(UUID.fromString(filterId), filterName, fragments)
-  }
-
-  private def descriptor(language: Locale) = {
-    val translatedText: ResourceBundle = ResourceBundle.getBundle(bundleName, language)
-    FilterDescriptorFragment(
-      displayName = translatedText.getString("displayName"),
-      description = translatedText.getString("description"),
-      previousConnection = FilterConnection(isPermitted = true),
-      nextConnection = FilterConnection(isPermitted = true),
-      parameters = List(
-        ListParameterDescriptor("fieldNames", translatedText.getString("fieldNames"), translatedText.getString("fieldNamesDescription"),
-          TextParameterDescriptor("field", translatedText.getString("fieldKeyNameHeader"), translatedText.getString("fieldKeyDescriptionHeader"))),
-        TextParameterDescriptor("pattern", translatedText.getString("patternKeyNameHeader"), translatedText.getString("patternKeyDescriptionHeader"))
-      ))
-  }
 }
 
-class GrokFilterLogic(context: StageContext, configuration: FilterConfiguration, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(context, configuration, shape) {
+class GrokFilterLogic(context: StageContext, configuration: Configuration, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(context, configuration, shape) {
 
   private val GROK_PATTERN: Regex = "\\(\\?<(\\w*)>".r
   private val NUMBER_PATTERN: Regex = "^[+-]?(\\d+(\\.\\d*)?|\\.\\d+)([eE][+-]?\\d+)?$".r
 
-  private var fieldNames = List.empty[String]
+  private var fieldNames = Seq.empty[String]
+  private var pattern = ""
   private var regex: Regex = "".r
 
-  override def configure(configuration: FilterConfiguration): Unit = {
-    configuration.parameters.foreach {
-      case TextListParameter("fieldNames", value) => fieldNames = value
-      case TextParameter("pattern", value) => regex = value.r(GROK_PATTERN.findAllMatchIn(value).map(_.group(1)).toSeq: _*)
-      case _ =>
-    }
+  override def configure(configuration: Configuration): Unit = {
+
+    fieldNames = configuration.getValueOrDefault(fieldNamesParameter, fieldNames)
+    pattern = configuration.getValueOrDefault(GrokFilterLogic.patternParameter, pattern)
+    regex = pattern.r(GROK_PATTERN.findAllMatchIn(pattern).map(_.group(1)).toSeq: _*)
   }
 
   override def onPush(): Unit = {
@@ -73,10 +91,10 @@ class GrokFilterLogic(context: StageContext, configuration: FilterConfiguration,
         if (fieldNames.contains(field.name) && field.value.isInstanceOf[TextValue]) {
           regex.findFirstMatchIn(field.value.asInstanceOf[TextValue].value).foreach(patternMatch =>
             patternMatch.groupNames.map(name => {
-                patternMatch.group(name) match {
-                  case value @ NUMBER_PATTERN(_*) => Field(name, DecimalValue(value.toDouble))
-                  case value => Field(name, TextValue(value))
-                }
+              patternMatch.group(name) match {
+                case value@NUMBER_PATTERN(_*) => Field(name, DecimalValue(value.toDouble))
+                case value => Field(name, TextValue(value))
+              }
             }).foldLeft(fields) { (fields, field) => fields += field })
         }
       }
