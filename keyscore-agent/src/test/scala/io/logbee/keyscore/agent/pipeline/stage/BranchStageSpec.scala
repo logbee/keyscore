fad77d855bdaa882a6ae4f2bd5a95dd89bf0df25
Elmar Schug <elmar.schug@jayware.org>
2018-09-08 12:05:38 +0200

    Fixed failing tests due to reuse of promises.

--- keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/stage/BranchStageSpec.scala
+++ keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/stage/BranchStageSpec.scala
@@ -10,7 +10,6 @@ import io.logbee.keyscore.commons.test.TestSystemWithMaterializerAndExecutionCon
 import io.logbee.keyscore.model.configuration.Configuration
 import io.logbee.keyscore.model.data.{Dataset, Field, Record, TextValue}
 import org.junit.runner.RunWith
-import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FreeSpec, Matchers}
@@ -19,46 +18,21 @@ import scala.concurrent.Promise
 import scala.language.postfixOps
 
 @RunWith(classOf[JUnitRunner])
-class BranchStageSpec extends FreeSpec with Matchers with ScalaFutures with MockFactory with TestSystemWithMaterializerAndExecutionContext {
+class BranchStageSpec extends FreeSpec with Matchers with ScalaFutures with TestSystemWithMaterializerAndExecutionContext {
 
   "A BranchStage" - {
 
     val context = StageContext(system, executionContext)
-
     val configurationA = Configuration()
     val configurationB = Configuration()
 
-    val updateConfiguration = Promise[Configuration]
-    val initializeConfiguration = Promise[Configuration]
-
-    val provider = (parameters: LogicParameters, shape: BranchShape[Dataset, Dataset, Dataset]) => new BranchLogic(parameters, shape) {
-
-      override def initialize(configuration: Configuration): Unit = {
-        initializeConfiguration.success(configuration)
-      }
-
-      override def configure(configuration: Configuration): Unit = {
-        updateConfiguration.success(configuration)
-      }
-
-      override def onPush(): Unit = {
-        val dataset = grab(in)
-        push(left, dataset)
-        push(right, dataset)
-      }
-
-      override def onPull(): Unit = {
-        if (isAvailable(left) && isAvailable(right)) {
-          pull(in)
-        }
-      }
-    }
-
     trait TestGraph {
 
-      val stage = new BranchStage(LogicParameters(UUID.randomUUID(), context, configurationA), provider)
+      val provider: (LogicParameters, BranchShape[Dataset, Dataset, Dataset]) => BranchLogic
 
-      val (source, branchFuture, left, right) = RunnableGraph.fromGraph(
+      lazy val stage = new BranchStage(LogicParameters(UUID.randomUUID(), context, configurationA), provider)
+
+      lazy val (source, branchFuture, left, right) = RunnableGraph.fromGraph(
         GraphDSL.create(TestSource.probe[Dataset], stage, TestSink.probe[Dataset], TestSink.probe[Dataset]) { (source, branch, left, right) =>
           (source, branch, left, right)
         } { implicit builder => (source, branch, left, right) =>
@@ -74,6 +48,24 @@ class BranchStageSpec extends FreeSpec with Matchers with ScalaFutures with Mock
 
     "should pass the appropriate configuration to it's logic" in new TestGraph {
 
+      val updateConfiguration = Promise[Configuration]
+      val initializeConfiguration = Promise[Configuration]
+
+      val provider = (parameters: LogicParameters, shape: BranchShape[Dataset, Dataset, Dataset]) => new BranchLogic(parameters, shape) {
+
+        override def initialize(configuration: Configuration): Unit = {
+          initializeConfiguration.success(configuration)
+        }
+
+        override def configure(configuration: Configuration): Unit = {
+          updateConfiguration.success(configuration)
+        }
+
+        override def onPush(): Unit = {}
+
+        override def onPull(): Unit = {}
+      }
+
       whenReady(branchFuture) { branch =>
         branch.configure(configurationB)
       }
@@ -88,6 +80,25 @@ class BranchStageSpec extends FreeSpec with Matchers with ScalaFutures with Mock
     }
 
     "should pass dataset to the left and right outputs" in new TestGraph {
+
+      val provider = (parameters: LogicParameters, shape: BranchShape[Dataset, Dataset, Dataset]) => new BranchLogic(parameters, shape) {
+
+        override def initialize(configuration: Configuration): Unit = {}
+
+        override def configure(configuration: Configuration): Unit = {}
+
+        override def onPush(): Unit = {
+          val dataset = grab(in)
+          push(left, dataset)
+          push(right, dataset)
+        }
+
+        override def onPull(): Unit = {
+          if (isAvailable(left) && isAvailable(right)) {
+            pull(in)
+          }
+        }
+      }
 
       val sample = Dataset(Record(Field("message", TextValue("The weather is cloudy with a current temperature of: -11.5 C"))))
 
--- keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/stage/MergeStageSpec.scala
+++ keyscore-agent/src/test/scala/io/logbee/keyscore/agent/pipeline/stage/MergeStageSpec.scala
@@ -10,7 +10,6 @@ import io.logbee.keyscore.commons.test.TestSystemWithMaterializerAndExecutionCon
 import io.logbee.keyscore.model.configuration.Configuration
 import io.logbee.keyscore.model.data._
 import org.junit.runner.RunWith
-import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FreeSpec, Matchers}
@@ -19,45 +18,21 @@ import scala.concurrent.Promise
 import scala.language.postfixOps
 
 @RunWith(classOf[JUnitRunner])
-class MergeStageSpec extends FreeSpec with Matchers with ScalaFutures with MockFactory with TestSystemWithMaterializerAndExecutionContext {
+class MergeStageSpec extends FreeSpec with Matchers with ScalaFutures with TestSystemWithMaterializerAndExecutionContext {
 
   "A MergeStage" - {
 
     val context = StageContext(system, executionContext)
-
     val configurationA = Configuration()
     val configurationB = Configuration()
 
-    val updateConfiguration = Promise[Configuration]
-    val initializeConfiguration = Promise[Configuration]
-
-    val provider = (parameters: LogicParameters, shape: MergeShape[Dataset, Dataset, Dataset]) => new MergeLogic(parameters, shape) {
-
-      override def initialize(configuration: Configuration): Unit = {
-        initializeConfiguration.success(configuration)
-      }
-
-      override def configure(configuration: Configuration): Unit = {
-        updateConfiguration.success(configuration)
-      }
-
-      override def onPush(): Unit = {
-        if (isAvailable(left) && isAvailable(right)) {
-          push(out, Dataset(grab(left).records ++ grab(right).records))
-        }
-      }
-
-      override def onPull(): Unit = {
-        if (!hasBeenPulled(left)) pull(left)
-        if (!hasBeenPulled(right)) pull(right)
-      }
-    }
-
     trait TestGraph {
 
-      val stage = new MergeStage(LogicParameters(UUID.randomUUID(), context, configurationA), provider)
+      val provider: (LogicParameters, MergeShape[Dataset, Dataset, Dataset]) => MergeLogic
 
-      val (left, right, mergeFuture, sink) = RunnableGraph.fromGraph(
+      lazy val stage = new MergeStage(LogicParameters(UUID.randomUUID(), context, configurationA), provider)
+
+      lazy val (left, right, mergeFuture, sink) = RunnableGraph.fromGraph(
         GraphDSL.create(TestSource.probe[Dataset], TestSource.probe[Dataset], stage, TestSink.probe[Dataset]) { (left, right, merge, sink) =>
           (left, right, merge, sink)
         } { implicit builder => (left, right, merge, sink) =>
@@ -73,6 +48,24 @@ class MergeStageSpec extends FreeSpec with Matchers with ScalaFutures with MockF
 
     "should pass the appropriate configuration to it's logic" in new TestGraph {
 
+      val updateConfiguration = Promise[Configuration]
+      val initializeConfiguration = Promise[Configuration]
+
+      val provider = (parameters: LogicParameters, shape: MergeShape[Dataset, Dataset, Dataset]) => new MergeLogic(parameters, shape) {
+
+        override def initialize(configuration: Configuration): Unit = {
+          initializeConfiguration.success(configuration)
+        }
+
+        override def configure(configuration: Configuration): Unit = {
+          updateConfiguration.success(configuration)
+        }
+
+        override def onPush(): Unit = {}
+
+        override def onPull(): Unit = {}
+      }
+
       whenReady(mergeFuture) { branch =>
         branch.configure(configurationB)
       }
@@ -87,6 +80,24 @@ class MergeStageSpec extends FreeSpec with Matchers with ScalaFutures with MockF
     }
 
     "should pass the merged dataset to the output" in new TestGraph {
+
+      val provider = (parameters: LogicParameters, shape: MergeShape[Dataset, Dataset, Dataset]) => new MergeLogic(parameters, shape) {
+
+        override def initialize(configuration: Configuration): Unit = {}
+
+        override def configure(configuration: Configuration): Unit = {}
+
+        override def onPush(): Unit = {
+          if (isAvailable(left) && isAvailable(right)) {
+            push(out, Dataset(grab(left).records ++ grab(right).records))
+          }
+        }
+
+        override def onPull(): Unit = {
+          if (!hasBeenPulled(left)) pull(left)
+          if (!hasBeenPulled(right)) pull(right)
+        }
+      }
 
       val sampleA = Dataset(Record(Field("message", TextValue("The weather is cloudy with a current temperature of: -11.5 C"))))
       val sampleB = Dataset(Record(Field("temperture", DecimalValue(-11.5))))
