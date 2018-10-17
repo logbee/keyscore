d9dbd6e0fa3ef42c8f441d6af7ed7a44cf4a112b
Elmar Schug <elmar.schug@jayware.org>
2018-10-17 09:01:42 +0200

    Implemented a first shot of the DescriptorApi for directives.

--- keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/filter/TextMutatorLogic.scala
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/filter/TextMutatorLogic.scala
@@ -4,27 +4,96 @@ import akka.stream.FlowShape
 import akka.stream.stage.StageLogging
 import io.logbee.keyscore.model.Described
 import io.logbee.keyscore.model.configuration.Configuration
-import io.logbee.keyscore.model.data.Dataset
+import io.logbee.keyscore.model.data.FieldValueType.Text
+import io.logbee.keyscore.model.data.{Dataset, FieldValueType}
 import io.logbee.keyscore.model.descriptor._
 import io.logbee.keyscore.model.localization.{Locale, Localization, TextRef}
 import io.logbee.keyscore.pipeline.contrib.CommonCategories
 import io.logbee.keyscore.model.util.ToOption.T2OptionT
 import io.logbee.keyscore.pipeline.api.{FilterLogic, LogicParameters}
 import io.logbee.keyscore.pipeline.contrib.CommonCategories.CATEGORY_LOCALIZATION
+import io.logbee.keyscore.model.descriptor.ToDescriptorRef.stringToDescriptorRef
 
 
 object TextMutatorLogic extends Described {
+
+  private val directiveSequence = FieldDirectiveSequenceParameterDescriptor(
+    ref = "textmutator.directiveSequence",
+    info = ParameterInfo(
+      displayName = "textmutator.directiveSequence.displayName",
+      description = "textmutator.directiveSequence.description"
+    ),
+    fieldTypes = Seq(Text),
+    directives = Seq(
+      trimDirective,
+      findAndReplaceDirective,
+      toTimestampDirective
+    )
+  )
+
+  private val trimDirective = FieldDirectiveDescriptor(
+    ref = DirectiveRef("ab31ce5f-d582-48e6-9a76-80436a733678"),
+    info = ParameterInfo(
+      displayName = "textmutator.trimDirective.displayName",
+      description = "textmutator.trimDirective.description"
+    ),
+  )
+
+  private val findAndReplaceDirective = FieldDirectiveDescriptor(
+    ref = DirectiveRef("dea6e8a9-7bf9-4af5-a049-fc9a567ab3b4"),
+    info = ParameterInfo(
+      displayName = "textmutator.findAndReplaceDirective.displayName",
+      description = "textmutator.findAndReplaceDirective.description"
+    ),
+    parameters = Seq(
+      TextParameterDescriptor(
+        ref = "textmutator.findAndReplaceDirective.find",
+        info = ParameterInfo(
+          displayName = "textmutator.findAndReplaceDirective.find.displayName",
+          description = "textmutator.findAndReplaceDirective.find.description"
+        ),
+        mandatory = true
+      ),
+      TextParameterDescriptor(
+        ref = "textmutator.findAndReplaceDirective.replace",
+        info = ParameterInfo(
+          displayName = "textmutator.findAndReplaceDirective.replace.displayName",
+          description = "textmutator.findAndReplaceDirective.replace.description"
+        ),
+        mandatory = true
+      )
+    )
+  )
+
+  private val toTimestampDirective = FieldDirectiveDescriptor(
+    ref = DirectiveRef("dea6e8a9-7bf9-4af5-a049-fc9a567ab3b4"),
+    info = ParameterInfo(
+      displayName = "textmutator.toTimestampDirective.displayName",
+      description = "textmutator.toTimestampDirective.description"
+    ),
+    parameters = Seq(
+      TextParameterDescriptor(
+        ref = "textmutator.toTimestampDirective.pattern",
+        info = ParameterInfo(
+          displayName = "textmutator.toTimestampDirective.pattern.displayName",
+          description = "textmutator.toTimestampDirective.pattern.description"
+        ),
+        mandatory = true
+      )
+    )
+  )
+
   override def describe = Descriptor(
     ref = "bf9c0ff2-64d5-44ed-9957-8128a50ab567",
     describes = FilterDescriptor(
       name = classOf[LoggerFilter].getName,
-      displayName = TextRef("displayName"),
-      description = TextRef("description"),
-      categories = Seq(CommonCategories.DEBUG),
-      parameters = Seq()
+      displayName = TextRef("textmutator.displayName"),
+      description = TextRef("textmutator.description"),
+      categories = Seq(CommonCategories.FIELDS),
+      parameters = Seq(directiveSequence)
     ),
     localization = Localization.fromResourceBundle(
-      bundleName = "io.logbee.keyscore.pipeline.contrib.filter.TextMutatorFilter",
+      bundleName = "io.logbee.keyscore.pipeline.contrib.filter.TextMutatorLogic",
       Locale.ENGLISH, Locale.GERMAN
     ) ++ CATEGORY_LOCALIZATION
   )
