05b10e22d2e0aa56a7bf95fc35d9029ec22a556d
Manuel Hinke <manuel.hinke@uni-ulm.de>
2018-10-28 14:17:36 +0100

    KS:M => reworked the data-type actions for better visibility.

--- keyscore-manager/src/app/live-editing/components/value-type.ts
+++ keyscore-manager/src/app/live-editing/components/value-type.ts
@@ -6,22 +6,22 @@ import {ValueJsonClass} from "../../models/dataset/Value";
     template: `
         <ng-container [ngSwitch]="type">
             <div *ngSwitchCase="jsonClass.TextValue" matTooltip="TextValue">
-                <mat-icon>text-icon</mat-icon>
+                <mat-icon svgIcon="text-icon"></mat-icon>
             </div>
             <div *ngSwitchCase="jsonClass.BooleanValue" matTooltip="BooleanValue">
-                <mat-icon>boolean-icon</mat-icon>
+                <mat-icon svgIcon="boolean-icon"></mat-icon>
             </div>
             <div *ngSwitchCase="jsonClass.NumberValue" matTooltip="NumberValue">
-                <mat-icon>number-icon</mat-icon>
+                <mat-icon svgIcon="number-icon">-icon</mat-icon>
             </div>
             <div *ngSwitchCase="jsonClass.DecimalValue" matTooltip="DecimalValue">
-                <mat-icon>decimal-icon</mat-icon>
+                <mat-icon svgIcon="decimal-icon"></mat-icon>
             </div>
             <div *ngSwitchCase="jsonClass.TimestampValue" matTooltip="TimestampValue">
-                <mat-icon>timestamp-icon</mat-icon>m
+                <mat-icon svgIcon="timestamp-icon"></mat-icon>m
             </div>
             <div *ngSwitchCase="jsonClass.DurationValue" matTooltip="DurationValue">
-                <mat-icon>duration-icon</mat-icon>
+                <mat-icon svgIcon="duration-icon"></mat-icon>
             </div>
         </ng-container>
     `
