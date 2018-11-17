58da402e902cb6b4d6bf230e326c9a457ee4d8ce
Elmar Schug <elmar.schug@jayware.org>
2018-11-17 16:15:48 +0100

    Fixed failing RetainFieldsFilterLogicSpec.

--- keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/filter/RetainFieldsFilterLogicSpec.scala
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/filter/RetainFieldsFilterLogicSpec.scala
@@ -5,20 +5,16 @@ import java.util.UUID
 import akka.stream.FlowShape
 import akka.stream.scaladsl.{Keep, Source}
 import akka.stream.testkit.scaladsl.{TestSink, TestSource}
-import io.logbee.keyscore.model.configuration.{Configuration, TextListParameter}
-import io.logbee.keyscore.model.data.Dataset
+import io.logbee.keyscore.model.configuration.{Configuration, FieldNameListParameter}
+import io.logbee.keyscore.model.data._
 import io.logbee.keyscore.pipeline.api.LogicParameters
 import io.logbee.keyscore.pipeline.api.stage.{FilterStage, StageContext}
-import io.logbee.keyscore.test.fixtures.ExampleData.{datasetMulti1, datasetMultiModified, datasetMultiModified2}
 import io.logbee.keyscore.test.fixtures.TestSystemWithMaterializerAndExecutionContext
 import org.junit.runner.RunWith
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 @RunWith(classOf[JUnitRunner])
 class RetainFieldsFilterLogicSpec extends WordSpec with Matchers with ScalaFutures with MockFactory with TestSystemWithMaterializerAndExecutionContext {
@@ -27,27 +23,32 @@ class RetainFieldsFilterLogicSpec extends WordSpec with Matchers with ScalaFutur
 
     val context = StageContext(system, executionContext)
 
-    val config1 = Configuration(
+    val config = Configuration(
       parameters = Seq(
-        TextListParameter(RetainFieldsFilterLogic.fieldNamesParameter.ref,Seq("bar", "bbq"))
+        FieldNameListParameter(RetainFieldsFilterLogic.fieldNamesParameter.ref, Seq("message", "temperature"))
       )
     )
 
     val provider = (parameters: LogicParameters, s: FlowShape[Dataset, Dataset]) =>
-      new RetainFieldsFilterLogic(LogicParameters(UUID.randomUUID(), context, config1), s)
+      new RetainFieldsFilterLogic(LogicParameters(UUID.randomUUID(), context, config), s)
 
-    val config2 = Configuration(
-      parameters = Seq(
-        TextListParameter(RetainFieldsFilterLogic.fieldNamesParameter.ref,Seq("foo", "notPresent"))
-      )
-    )
+    val filterStage = new FilterStage(LogicParameters(UUID.randomUUID(), context, config), provider)
 
-    val filterStage = new FilterStage(LogicParameters(UUID.randomUUID(), context, config1),provider)
-
-   val ((source,filterFuture), sink) = Source.fromGraph(TestSource.probe[Dataset])
+    val ((source, filterFuture), sink) = Source.fromGraph(TestSource.probe[Dataset])
       .viaMat(filterStage)(Keep.both)
       .toMat(TestSink.probe[Dataset])(Keep.both)
       .run()
+
+    val sample = Dataset(Record(
+      Field("message", TextValue("Hello World!")),
+      Field("temperature", DecimalValue(11.5)),
+      Field("fubar", TextValue("Delete Me!"))
+    ))
+
+    val modifiedSample = Dataset(Record(
+      Field("message", TextValue("Hello World!")),
+      Field("temperature", DecimalValue(11.5))
+    ))
   }
 
   "A RetainFieldsFilter" should {
@@ -57,17 +58,12 @@ class RetainFieldsFilterLogicSpec extends WordSpec with Matchers with ScalaFutur
     }
 
     "retain only the specified fields and remove all others" in new TestStream {
+
       whenReady(filterFuture) { filter =>
-        source.sendNext(datasetMulti1)
+
+        source.sendNext(sample)
         sink.request(1)
-        sink.expectNext(datasetMultiModified)
-
-        Await.ready(filter.configure(config2), 10 seconds)
-
-        source.sendNext(datasetMulti1)
-        sink.request(1)
-        sink.expectNext(datasetMultiModified2)
-
+        sink.expectNext(modifiedSample)
       }
     }
   }
