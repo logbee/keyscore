95febbb323dac26f1205636462a2293787c9ccd8
Elmar Schug <elmar.schug@jayware.org>
2018-12-01 03:35:41 +0100

    Implemented a CombineByValueLogic to gather records containing a field with the same value.

---
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/filter/CombineByValueLogic.scala
@@ -0,0 +1,129 @@
+package io.logbee.keyscore.pipeline.contrib.filter
+
+import akka.stream.FlowShape
+import akka.stream.stage.StageLogging
+import io.logbee.keyscore.model.Described
+import io.logbee.keyscore.model.configuration.Configuration
+import io.logbee.keyscore.model.data.{Dataset, Field, Record}
+import io.logbee.keyscore.model.descriptor._
+import io.logbee.keyscore.model.json4s.KeyscoreFormats
+import io.logbee.keyscore.model.localization.{Locale, Localization, TextRef}
+import io.logbee.keyscore.model.util.ToOption.T2OptionT
+import io.logbee.keyscore.pipeline.api.{FilterLogic, LogicParameters}
+import io.logbee.keyscore.pipeline.contrib.{CommonCategories, LoggerLogic}
+import org.json4s.Formats
+
+import scala.collection.mutable
+
+object CombineByValueLogic extends Described {
+
+  val fieldNameParameter = FieldNameParameterDescriptor(
+    ref = "simple-aggregate.fieldName",
+    info = ParameterInfo(
+      displayName = "fieldName.displayName",
+      description = "fieldName.description"
+    ),
+    hint = FieldNameHint.PresentField,
+    mandatory = true
+  )
+
+  override def describe = Descriptor(
+    ref = "efbb3b8e-35f4-45ac-87be-f454cf3a951c",
+    describes = FilterDescriptor(
+      name = classOf[LoggerLogic].getName,
+      displayName = TextRef("displayName"),
+      description = TextRef("description"),
+      categories = Seq(CommonCategories.BATCH_COMPOSITION),
+      parameters = Seq(fieldNameParameter),
+      icon = Icon.fromClass(classOf[LoggerLogic])
+    ),
+    localization = Localization.fromResourceBundle(
+      bundleName = "io.logbee.keyscore.pipeline.contrib.filter.CombineByValueLogic",
+      Locale.ENGLISH, Locale.GERMAN
+    ) ++ CommonCategories.CATEGORY_LOCALIZATION
+  )
+}
+
+class CombineByValueLogic(parameters: LogicParameters, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(parameters, shape) with StageLogging {
+
+  private implicit val jsonFormats: Formats = KeyscoreFormats.formats
+
+  private var fieldName = ""
+
+  private var current: Option[(Field, mutable.ListBuffer[Dataset])] = None
+  private var next: Option[(Field, Dataset)] = None
+  private var pass: Option[Dataset] = None
+
+  override def initialize(configuration: Configuration): Unit = {
+    configure(configuration)
+  }
+
+  override def configure(configuration: Configuration): Unit = {
+    fieldName = configuration.getValueOrDefault(CombineByValueLogic.fieldNameParameter, fieldName)
+  }
+
+  override def onPush(): Unit = {
+
+    val dataset = grab(in)
+
+    val field = dataset.records.flatMap(record => record.fields).find(field => field.name == fieldName)
+
+    if (isFirstMatch(field)) {
+      current = Option((field.get, mutable.ListBuffer(dataset)))
+    }
+    else if (isAnotherMatch(field)) {
+      current.get._2 += dataset
+    }
+    else if (isNewMatch(field)) {
+      next = Option((field.get, dataset))
+    }
+    else {
+      pass = Option(dataset)
+    }
+
+    pushOutIfAvailable()
+  }
+
+  override def onPull(): Unit = {
+    pushOutIfAvailable()
+  }
+
+  private def pushOutIfAvailable(): Unit = {
+
+    if (!isAvailable(out)) {
+      return
+    }
+
+    if (pass.isDefined) {
+      push(out, pass.get)
+      pass = None
+    }
+    else if (next.isDefined) {
+      val datasets = current.get._2
+      val records = datasets.flatMap(d => d.records).toList
+      push(out, Dataset(datasets.head.metadata, records))
+      swapCurrentAndNext()
+    }
+
+    if (!hasBeenPulled(in)) {
+      pull(in)
+    }
+  }
+
+  private def isNewMatch(field: Option[Field]) = {
+    field.isDefined && current.isDefined && field.get != current.get._1 && field.get.name == fieldName
+  }
+
+  private def isAnotherMatch(field: Option[Field]) = {
+    field.isDefined && current.isDefined && field.get == current.get._1
+  }
+
+  private def isFirstMatch(field: Option[Field]) = {
+    field.isDefined && current.isEmpty && field.get.name == fieldName
+  }
+
+  private def swapCurrentAndNext(): Unit = {
+    current = Option(next.get._1, mutable.ListBuffer(next.get._2))
+    next = None
+  }
+}
---
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/filter/CombineByValueLogicSpec.scala
@@ -0,0 +1,131 @@
+package io.logbee.keyscore.pipeline.contrib.filter
+
+import java.util.UUID.randomUUID
+
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Keep, Source}
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
+import io.logbee.keyscore.model.configuration.{Configuration, FieldNameParameter}
+import io.logbee.keyscore.model.data.{Record, _}
+import io.logbee.keyscore.pipeline.api.LogicParameters
+import io.logbee.keyscore.pipeline.api.stage.{FilterStage, StageContext}
+import io.logbee.keyscore.test.fixtures.TestSystemWithMaterializerAndExecutionContext
+import org.junit.runner.RunWith
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FreeSpec, Matchers}
+
+import scala.concurrent.duration._
+
+
+@RunWith(classOf[JUnitRunner])
+class CombineByValueLogicSpec extends FreeSpec with ScalaFutures with Matchers with TestSystemWithMaterializerAndExecutionContext {
+
+  trait TestStream {
+
+    val configuration = Configuration(parameters = Seq(
+      FieldNameParameter(CombineByValueLogic.fieldNameParameter.ref, "key")
+    ))
+
+    val context = StageContext(system, executionContext)
+    val filterStage = new FilterStage(LogicParameters(randomUUID(), context, configuration), (p: LogicParameters, s: FlowShape[Dataset, Dataset]) => new CombineByValueLogic(p, s))
+
+    val ((source, filterFuture), sink) = Source.fromGraph(TestSource.probe[Dataset])
+      .viaMat(filterStage)(Keep.both)
+      .toMat(TestSink.probe[Dataset])(Keep.both)
+      .run()
+  }
+
+  "A CombineByValueLogic" - {
+
+    val samplesA = Seq(
+      Dataset(Record(
+        Field("key", TextValue("weather-forecast")),
+        Field("message", TextValue("Its a cloudy day!"))
+      )),
+      Dataset(
+        Record(
+          Field("key", TextValue("weather-forecast")),
+          Field("temperature", DecimalValue(23.5))
+        ),
+        Record(
+          Field("sensor", TextValue("XC90")),
+        )
+      )
+    )
+
+    val otherSample = Dataset(Record(
+      Field("key", TextValue("daily-news")),
+      Field("message", TextValue("No news today."))
+    ))
+
+    "should let records pass which does not contain the configured field" in new TestStream {
+
+      val samples = Seq(
+        Dataset(Record(
+          Field("message", TextValue("This is an unknown message."))
+        )),
+        Dataset(Record(
+          Field("foo", TextValue("bar"))
+        ))
+      )
+
+      whenReady(filterFuture) { _ =>
+        samples.foreach(source.sendNext)
+        sink.request(3)
+        sink.requestNext(samples.head)
+        sink.requestNext(samples.last)
+        sink.expectNoMessage(2 seconds)
+      }
+    }
+
+    "should not let records pass when the value of the configured field does not change" in new TestStream {
+
+      whenReady(filterFuture) { filter =>
+        samplesA.foreach(source.sendNext)
+        sink.request(1)
+        sink.expectNoMessage(2 seconds)
+      }
+    }
+
+    "should combine consecutive datasets by the value of the configured field" in new TestStream {
+
+      whenReady(filterFuture) { filter =>
+
+        sink.request(3)
+
+        samplesA.foreach(source.sendNext)
+
+        sink.expectNoMessage(1 seconds)
+
+        source.sendNext(otherSample)
+
+        var actual = sink.requestNext()
+
+        actual.records should contain only (
+          Record(
+            Field("key", TextValue("weather-forecast")),
+            Field("message", TextValue("Its a cloudy day!")),
+          ),
+          Record(
+            Field("key", TextValue("weather-forecast")),
+            Field("temperature", DecimalValue(23.5))
+          ),
+          Record(
+            Field("sensor", TextValue("XC90")),
+          )
+        )
+
+        sink.expectNoMessage(1 seconds)
+
+        samplesA.foreach(source.sendNext)
+
+        actual = sink.requestNext()
+
+        actual shouldBe otherSample
+
+        sink.expectNoMessage(1 seconds)
+      }
+    }
+  }
+}
