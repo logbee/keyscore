42642c3defdcbf445b5dc2474e33ee99baa565b1
Zed Harper <zedharper@tutanota.com>
2018-12-21 09:39:48 +0100

    KS:P Tailin -> Refactored TailinSourceLogicSpec to be easier to extend.

--- keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/tailin/TailinSourceLogicSpec.scala
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/tailin/TailinSourceLogicSpec.scala
@@ -4,7 +4,6 @@ import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path, StandardOpenOption}
 import java.util.UUID
-import java.util.UUID.randomUUID
 
 import akka.stream.SourceShape
 import akka.stream.scaladsl.{Keep, Source}
@@ -24,138 +23,190 @@ import scala.concurrent.duration._
 import io.logbee.keyscore.pipeline.contrib.tailin.util.TestUtil
 import io.logbee.keyscore.pipeline.contrib.tailin.file.ReadMode
 import io.logbee.keyscore.model.configuration.NumberParameter
+import io.logbee.keyscore.pipeline.contrib.tailin.file.ReadMode._
+import java.nio.charset.Charset
+import io.logbee.keyscore.model.conversion.TextValueConversion
 
 @RunWith(classOf[JUnitRunner])
 class TailinSourceLogicSpec extends FreeSpec with Matchers with BeforeAndAfter with BeforeAndAfterAll with ScalaFutures with TestSystemWithMaterializerAndExecutionContext {
   
   var watchDir: Path = _
-  var persistenceFile: File = _ //TODO actually pass this persistence file along in a parameter to be able to delete it during tests and maybe be easier to configure (though we probably don't actually want to expose this to the user
   
   before {
     watchDir = Files.createTempDirectory("watchTest")
-
     TestUtil.waitForFileToExist(watchDir.toFile)
-
-    persistenceFile = new File(".keyscoreFileTailinPersistence_test")
-    persistenceFile.createNewFile()
-    TestUtil.waitForFileToExist(persistenceFile)
   }
-  
   after {
     TestUtil.recursivelyDelete(watchDir)
-    persistenceFile.delete()
   }
   
-  override def afterAll {
+  override def afterAll = {
+    
     TestKit.shutdownActorSystem(system)
-  }
-  
-  
-  trait DefaultTailinSourceValues {
-    val bufferSize = 1024
-    
-    val context = StageContext(system, executionContext)
-    
-    
-    val configuration = Configuration(
-      TextParameter(  TailinSourceLogic.filePattern.ref,     watchDir.toString + "/tailin.csv"),
-      ChoiceParameter(TailinSourceLogic.readMode.ref,        ReadMode.LINE.toString),
-      ChoiceParameter(TailinSourceLogic.encoding.ref,        StandardCharsets.UTF_8.toString),
-      TextParameter(  TailinSourceLogic.rotationSuffix.ref,  ".[1-5]"),
-      TextParameter(  TailinSourceLogic.fieldName.ref,       "output"),
-    )
-    
-    val provider = (parameters: LogicParameters, shape: SourceShape[Dataset]) => new TailinSourceLogic(LogicParameters(randomUUID(), context, configuration), shape)
-    
-    val sourceStage = new SourceStage(LogicParameters(UUID.randomUUID(), context, configuration), provider)
-    
-    val (sourceFuture, sink) = Source.fromGraph(sourceStage).toMat(TestSink.probe[Dataset])(Keep.both).run()
   }
   
   
   
   "A TailinSource" - {
     
-    "should push one available string for one available pull" in new DefaultTailinSourceValues {
-      
-      val text = "Hallo Welt"
-      val file = TestUtil.createFile(watchDir, "tailin.csv", text)
-      
-      sink.request(1)
-      var result = sink.expectNext()
-      result.records.head.fields should have size 1
-      result.records.head.fields.head.value shouldEqual TextValue(text)
-    }
+    case class FileWithContent(path: String, lines: Seq[String])
+
+    case class TestSetup(
+        files: Seq[FileWithContent],
+        filePattern: String,
+        readMode: ReadMode,
+        encoding: Charset = StandardCharsets.UTF_8,
+        rotationSuffix: String = "",
+        expectedData: Seq[String],
+    )
+    
+    val testSetups = Seq(
+      TestSetup(
+          files = Seq(FileWithContent(path="tailin.csv", lines=Seq("abcde", "fghij", "klmno"))),
+          filePattern = "tailin.csv",
+          readMode = ReadMode.LINE,
+          expectedData = Seq("abcde", "fghij", "klmno"),
+      ),
+      TestSetup(
+          files = Seq(FileWithContent(path="tailin.csv", lines=Seq("abcde", "fghij", "klmno"))),
+          filePattern = "tailin.csv",
+          readMode = ReadMode.FILE,
+          expectedData = Seq("abcde\n", "fghij\n", "klmno\n"),
+      ),
+//      TestSetup( //TODO this test-case (UTF-16) currently still fails, because zero-width Byte Order Marks are inserted into the string. Research how to best handle these.
+//          files = Seq(FileWithContent(path="tailin.csv", lines=Seq("abcde", "fghij", "klmnö"))),
+//          filePattern = "tailin.csv",
+//          readMode = ReadMode.LINE,
+//          encoding = StandardCharsets.UTF_16,
+//          expectedData = Seq("abcde", "fghij", "klmnö"),
+//      ),
+    )
     
     
     
-    "should push multiple available strings" - {
+    testSetups.foreach { testSetup =>
       
-      Seq(true, false).foreach { waitFor_DirWatcher_processEvents =>
-        (if (waitFor_DirWatcher_processEvents == true)
-           "when it has to wait for pulls (buffering)"
-         else
-           "when pulls are made as it reads the data"
-        ) in new DefaultTailinSourceValues {
+      s"""with
+        filePattern:    "${testSetup.filePattern}"
+        readMode:       "${testSetup.readMode}"
+        encoding:       "${testSetup.encoding}"
+        rotationSuffix: "${testSetup.rotationSuffix}"""" - {
+        
+        
+        trait DefaultTailinSourceValues {
           
-          val file = TestUtil.createFile(watchDir, "tailin.csv", "")
+          val context = StageContext(system, executionContext)
           
-          val texts = Seq("abcde", "fghij", "klmno")
+          val configuration = Configuration(
+            TextParameter(  TailinSourceLogic.filePattern.ref,     watchDir + "/" + testSetup.filePattern),
+            ChoiceParameter(TailinSourceLogic.readMode.ref,        testSetup.readMode.toString),
+            ChoiceParameter(TailinSourceLogic.encoding.ref,        testSetup.encoding.toString),
+            TextParameter(  TailinSourceLogic.rotationSuffix.ref,  testSetup.rotationSuffix),
+            TextParameter(  TailinSourceLogic.fieldName.ref,       "output"),
+          )
           
-          texts.foreach { text =>
-            TestUtil.writeStringToFile(file, text + "\n", StandardOpenOption.APPEND)
-            
-            if (waitFor_DirWatcher_processEvents) {
-              Thread.sleep(1500)
+          val provider = (parameters: LogicParameters, shape: SourceShape[Dataset]) => new TailinSourceLogic(LogicParameters(UUID.randomUUID, context, configuration), shape)
+          
+          val sourceStage = new SourceStage(LogicParameters(UUID.randomUUID, context, configuration), provider)
+          
+          val (sourceFuture, sink) = Source.fromGraph(sourceStage).toMat(TestSink.probe[Dataset])(Keep.both).run()
+        }
+        
+        
+        
+        "should push one available string for one available pull" in new DefaultTailinSourceValues {
+          
+          val file = TestUtil.createFile(watchDir, testSetup.files.head.path)
+          
+          val text = testSetup.files.head.lines.head
+          TestUtil.writeStringToFile(file, text + "\n", encoding=testSetup.encoding)
+          
+          sink.request(1)
+          var result = sink.expectNext()
+          result.records.head.fields should have size 1
+          result.records.head.fields.head.value shouldEqual TextValue(testSetup.expectedData.head)
+        }
+        
+        
+        "should push multiple available strings" - {
+          
+          Seq(false).foreach { waitFor_DirWatcher_processEvents =>
+            (if (waitFor_DirWatcher_processEvents == true)
+               "when it has to wait for pulls (buffering)"
+             else
+               "when pulls are made as it reads the data"
+            ) in new DefaultTailinSourceValues {
+              
+              val file = TestUtil.createFile(watchDir, "tailin.csv")
+              
+              val texts = testSetup.files.head.lines
+              
+              texts.foreach { text =>
+                TestUtil.writeStringToFile(file, text + "\n", encoding=testSetup.encoding)
+                
+                if (waitFor_DirWatcher_processEvents) {
+                  Thread.sleep(1500)
+                }
+              }
+              
+              
+              
+              var concatenatedExpectedData = testSetup.expectedData.fold("")((string1, string2) => string1 + string2)
+              
+              sink.request(texts.size)
+              
+              var concatenatedReturnedData = ""
+              
+              for (i <- 1 to texts.size) {
+                val datasets: Seq[Dataset] = sink.receiveWithin(max=3.seconds, messages=texts.size)
+
+                datasets.foreach { dataset =>
+                  concatenatedReturnedData += dataset.records.head.fields.head.value.asInstanceOf[TextValue].value
+                }
+              }
+              
+              concatenatedReturnedData shouldEqual concatenatedExpectedData
             }
           }
+        }
+        
+        
+        "should push multiple strings that become available in a delayed manner for multiple delayed pulls" in new DefaultTailinSourceValues {
           
+          val file = TestUtil.createFile(watchDir, "tailin.csv")
           
-          sink.request(3)
+          val texts = testSetup.files.head.lines
           
-          texts.foreach { text =>
+          texts.zip(testSetup.expectedData).foreach { case (text, expectedText) =>
+            TestUtil.writeStringToFile(file, text + "\n", encoding=testSetup.encoding)
+            
+            sink.request(1)
+            
+            Thread.sleep(1500) //wait for processEvents to trigger once
+            
             val datasetText = sink.expectNext()
-            datasetText.records.head.fields.head.value shouldEqual TextValue(text)
+            
+            datasetText.records.head.fields.head.value shouldEqual TextValue(expectedText)
           }
         }
+        
+        
+        "should wait for strings to become available if no strings are available when it gets pulled" in new DefaultTailinSourceValues {
+          
+          val file = TestUtil.createFile(watchDir, "tailin.csv")
+          
+          sink.request(1)
+          
+          Thread.sleep(3000)
+          
+          val text = testSetup.files.head.lines.head
+          TestUtil.writeStringToFile(file, text + "\n", encoding=testSetup.encoding)
+          
+          val datasetText = sink.expectNext()
+          
+          datasetText.records.head.fields.head.value shouldEqual TextValue(testSetup.expectedData.head)
+        }
       }
-    }
-    
-    
-    "should push multiple strings that become available in a delayed manner for multiple delayed pulls" in new DefaultTailinSourceValues {
-      
-      val file = TestUtil.createFile(watchDir, "tailin.csv", "")
-      
-      val texts = Seq("abcde", "fghij", "klmno")
-      
-      texts.foreach { text => 
-        TestUtil.writeStringToFile(file, text + "\n", StandardOpenOption.APPEND)
-        
-        sink.request(1)
-        
-        Thread.sleep(1500) //wait for processEvents to trigger once
-        
-        val datasetText = sink.expectNext()
-        
-        datasetText.records.head.fields.head.value shouldEqual TextValue(text)
-      }
-    }
-    
-    
-    "should wait for strings to become available if no strings are available when it gets pulled" in new DefaultTailinSourceValues {
-      
-      val file = TestUtil.createFile(watchDir, "tailin.csv", "")
-      
-      sink.request(1)
-      
-      Thread.sleep(5000)
-      
-      val text = "test"
-      TestUtil.writeStringToFile(file, text + "\n", StandardOpenOption.APPEND)
-      
-      val datasetText = sink.expectNext()
-      
-      datasetText.records.head.fields.head.value shouldEqual TextValue(text)
     }
   }
 }
