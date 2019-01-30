c21c9332666ad9ddfd7b159cc9949de4ebf8b22f
Zed Harper <zedharper@tutanota.com>
2019-01-30 18:02:38 +0100

    KS:P Tailin -> Implement ReadPersistence as a common frontend for the persistence of completed reads and committed reads.

---
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/tailin/persistence/ReadPersistence.scala
@@ -0,0 +1,79 @@
+package io.logbee.keyscore.pipeline.contrib.tailin.persistence
+
+import java.io.File
+import io.logbee.keyscore.pipeline.contrib.tailin.read.FileReadRecord
+import scala.reflect.runtime.universe._
+
+
+class ReadPersistence(completedPersistence: PersistenceContext, committedPersistence: PersistenceContext) {
+  
+  
+  def getCompletedRead(file: File): FileReadRecord = {
+    
+    var nextRead = FileReadRecord(previousReadPosition=0, previousReadTimestamp=0)
+    
+    val readPersistenceContextEntryOpt = completedPersistence.load[FileReadRecord](file.getAbsolutePath)
+    if (readPersistenceContextEntryOpt != None) {
+      val readPersistenceContextEntry = readPersistenceContextEntryOpt.get
+      
+      nextRead = FileReadRecord(previousReadPosition=readPersistenceContextEntry.previousReadPosition,
+                                previousReadTimestamp=readPersistenceContextEntry.previousReadTimestamp)
+    }
+    else { //if no completed, uncommitted reads have been persisted
+      val commitPersistenceContextEntryOpt = committedPersistence.load[FileReadRecord](file.getAbsolutePath)
+      if (commitPersistenceContextEntryOpt != None) {
+        val commitPersistenceContextEntry = commitPersistenceContextEntryOpt.get
+        
+        nextRead = FileReadRecord(previousReadPosition=commitPersistenceContextEntry.previousReadPosition,
+                                  previousReadTimestamp=commitPersistenceContextEntry.previousReadTimestamp)
+      }
+    }
+    
+    nextRead
+  }
+  
+  
+  def completeRead(file: File, fileReadRecord: FileReadRecord) = {
+    
+    val readPersistenceEntryOpt = completedPersistence.load[FileReadRecord](file.getAbsolutePath)(typeTag[FileReadRecord])
+    readPersistenceEntryOpt match {
+      case Some(readPersistenceEntry) =>
+        if (readPersistenceEntry.previousReadTimestamp < fileReadRecord.previousReadTimestamp) {
+          completedPersistence.store(file.getAbsolutePath, fileReadRecord)
+        }
+        
+      case None =>
+        completedPersistence.store(file.getAbsolutePath, fileReadRecord)
+    }
+  }
+  
+  
+  def commitRead(file: File, fileReadRecord: FileReadRecord) = {
+    
+    { //check if the timestamp to commit is for some reason newer than the timestamp of the last completed read
+      val readPersistenceEntryOpt = completedPersistence.load[FileReadRecord](file.getAbsolutePath)(typeTag[FileReadRecord])
+      readPersistenceEntryOpt match {
+        case Some(readPersistenceEntry) =>
+          if (readPersistenceEntry.previousReadTimestamp < fileReadRecord.previousReadTimestamp) {
+            completedPersistence.store(file.getAbsolutePath, fileReadRecord)
+          }
+          
+        case None =>
+          completedPersistence.store(file.getAbsolutePath, fileReadRecord)
+      }
+    }
+    
+    
+    //make sure the entry to commit is actually newer than the entry we currently have committed
+    val commitPersistenceEntryOpt = committedPersistence.load[FileReadRecord](file.getAbsolutePath)(typeTag[FileReadRecord])
+    commitPersistenceEntryOpt match {
+      case Some(commitPersistenceEntry) =>
+        if (commitPersistenceEntry.previousReadTimestamp < fileReadRecord.previousReadTimestamp) {
+          committedPersistence.store(file.getAbsolutePath, fileReadRecord)
+        }
+        
+      case None =>
+        committedPersistence.store(file.getAbsolutePath, fileReadRecord)
+    }
+  }
+}
---
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/tailin/persistence/ReadPersistenceSpec.scala
@@ -0,0 +1,196 @@
+package io.logbee.keyscore.pipeline.contrib.tailin.persistence
+
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers
+import org.scalamock.scalatest.MockFactory
+import io.logbee.keyscore.pipeline.contrib.tailin.read.FileReadRecord
+import scala.reflect.runtime.universe._
+import java.io.File
+
+class ReadPersistenceSpec extends FreeSpec with Matchers with MockFactory {
+  
+  
+  trait ReadPersistenceSetup {
+    val completedPersistence = mock[PersistenceContext]
+    val committedPersistence = mock[PersistenceContext]
+    
+    val readPersistence = new ReadPersistence(completedPersistence, committedPersistence)
+    
+    val file = new File(".testFile")
+    val fileReadRecord = FileReadRecord(previousReadPosition=1, previousReadTimestamp=2)
+  }
+  
+  
+  "A ReadPersistence should" - {
+    
+    "return the last read entry" - {
+      
+      "when no read has been completed or committed" in
+      new ReadPersistenceSetup {
+        
+        inSequence {
+          (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+            .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+            .returning(None)
+          
+          (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+            .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+            .returning(None)
+        }
+        
+        readPersistence.getCompletedRead(file) shouldBe FileReadRecord(previousReadPosition=0, previousReadTimestamp=0)
+      }
+      
+      
+      "when no read has been completed, but one has been committed" in
+      new ReadPersistenceSetup {
+        
+        inSequence {
+          (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+            .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+            .returning(None)
+          
+          (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+            .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+            .returning(Some(fileReadRecord))
+        }
+        
+        readPersistence.getCompletedRead(file) shouldBe fileReadRecord
+      }
+      
+      
+      "when a read has been completed" in
+      new ReadPersistenceSetup {
+        
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord))
+        
+        readPersistence.getCompletedRead(file) shouldBe fileReadRecord
+      }
+    }
+    
+    
+    "store a completed read" in
+    new ReadPersistenceSetup {
+      
+      (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(None)
+      
+      (completedPersistence.store _)
+        .expects(file.getAbsolutePath, fileReadRecord)
+      
+      readPersistence.completeRead(file, fileReadRecord)
+    }
+    
+    
+    "store a committed read" in
+    new ReadPersistenceSetup {
+      
+      inSequence {
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord))
+        
+        (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(None)
+        
+        (committedPersistence.store _)
+          .expects(file.getAbsolutePath, fileReadRecord)
+      }
+      
+      readPersistence.commitRead(file, fileReadRecord)
+    }
+    
+    
+    "store a committed read and update the list of completed reads, if that for some reason has a less up-to-date entry" in
+    new ReadPersistenceSetup {
+      
+      inSequence {
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(None)
+        
+        (completedPersistence.store _)
+          .expects(file.getAbsolutePath, fileReadRecord)
+        
+        (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(None)
+        
+        (committedPersistence.store _)
+          .expects(file.getAbsolutePath, fileReadRecord)
+      }
+      
+      readPersistence.commitRead(file, fileReadRecord)
+    }
+    
+    
+    "discard a completed read which is older than the current entry" in
+    new ReadPersistenceSetup {
+      
+      inSequence {
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord.copy(previousReadTimestamp=fileReadRecord.previousReadTimestamp+1)))
+        
+        (completedPersistence.store _)
+          .expects(*, *)
+          .never
+      }
+      
+      readPersistence.completeRead(file, fileReadRecord)
+    }
+    
+    
+    "discard a committed read which is older than the current entry" in
+    new ReadPersistenceSetup {
+      
+      inSequence {
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord.copy(previousReadTimestamp=fileReadRecord.previousReadTimestamp+1))) //this does not need to be updated
+        
+        (completedPersistence.store _)
+          .expects(*, *)
+          .never
+        
+        (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord.copy(previousReadTimestamp=fileReadRecord.previousReadTimestamp+1))) //this does not need to be updated
+        
+        (committedPersistence.store _)
+          .expects(*, *)
+          .never
+      }
+      
+      readPersistence.commitRead(file, fileReadRecord)
+    }
+    
+    
+    "discard a committed read which is older than the current entry, while updating the out-of-date completed read persistence" in
+    new ReadPersistenceSetup {
+      
+      inSequence {
+        (completedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord.copy(previousReadTimestamp=fileReadRecord.previousReadTimestamp-1))) //this needs to be updated
+        
+        (completedPersistence.store _)
+          .expects(file.getAbsolutePath, fileReadRecord)
+        
+        (committedPersistence.load[FileReadRecord] (_: String)(_: TypeTag[FileReadRecord]))
+          .expects(file.getAbsolutePath, typeTag[FileReadRecord])
+          .returning(Some(fileReadRecord.copy(previousReadTimestamp=fileReadRecord.previousReadTimestamp+1))) //this does not need to be updated
+        
+        (committedPersistence.store _)
+          .expects(*, *)
+          .never
+      }
+        
+      readPersistence.commitRead(file, fileReadRecord)
+    }
+  }
+}
