1cb992820d05ee4f9e681b7442545922e8971925
Zed Harper <zedharper@tutanota.com>
2019-01-31 14:46:27 +0100

    KS:P Tailin -> Clean up code in FileReaderManager

--- keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/tailin/read/FileReaderManager.scala
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/tailin/read/FileReaderManager.scala
@@ -25,6 +25,9 @@ class FileReaderManager(readSchedule: ReadSchedule, fileReaderProvider: FileRead
     val readScheduleItemOpt = readSchedule.pop()
     
     readScheduleItemOpt match {
+      case None => //if no reads scheduled
+        //do nothing, rescheduling is done by caller
+        
       case Some(readScheduleItem) =>
         
         val baseFile = readScheduleItem.file
@@ -42,25 +45,6 @@ class FileReaderManager(readSchedule: ReadSchedule, fileReaderProvider: FileRead
         
         
         fileReader.read(callback, readScheduleItem)
-        
-      case None =>//if no reads scheduled
-       //TODO schedule a dirWatcher.processEvents and a retry
-       // -> scheduling a retry can only be done in TailinSourceLogic (?)
-        // -> we might not have to do anything here
-       
-       
-       //TODO maybe pass this method a callback that will doPush()
-       // that way it's non-blocking, and can at any moment when ready do the push
-       // and therefore could be timered every 1 second or something
-       //  Can we then guarantee, though, that it doesn't push twice?
-       //  I think, we can, because only one timeline is going to be active
     }
-    
-    
-    //TODO do the black magic where you find out the correct FileReader
-    // maybe only do half the black magic here
-    // and half the black magic in a RotationReaderHandler-class
-    //  let's do the 50% solution first, where we pass it directly to the FileReader and assume no rotation
-    //  the RotationReaderHandler-class should be easy to introduce in between
   }
 }
