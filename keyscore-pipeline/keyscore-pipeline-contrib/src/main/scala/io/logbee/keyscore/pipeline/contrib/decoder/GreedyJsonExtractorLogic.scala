bcde872bd925946b3c816c613de0023c38f3591d
Elmar Schug <elmar.schug@jayware.org>
2019-02-19 08:50:13 +0100

    Implemented a GreedyJsonExtractor to extract arbitrary json from text-fields.

---
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/main/scala/io/logbee/keyscore/pipeline/contrib/decoder/GreedyJsonExtractorLogic.scala
@@ -0,0 +1,112 @@
+package io.logbee.keyscore.pipeline.contrib.decoder
+
+import java.awt.Color
+
+import akka.stream.FlowShape
+import akka.stream.stage.StageLogging
+import io.logbee.keyscore.model.Described
+import io.logbee.keyscore.model.configuration.Configuration
+import io.logbee.keyscore.model.data.{Dataset, Record}
+import io.logbee.keyscore.model.descriptor.Maturity.Experimental
+import io.logbee.keyscore.model.descriptor._
+import io.logbee.keyscore.model.localization.{Locale, Localization, TextRef}
+import io.logbee.keyscore.model.util.ToOption.T2OptionT
+import io.logbee.keyscore.pipeline.api.{FilterLogic, LogicParameters}
+import io.logbee.keyscore.pipeline.contrib.CommonCategories._
+import io.logbee.keyscore.pipeline.contrib.decoder.GreedyJsonExtractorLogic.{fieldNameParameter, prefixParameter, removeFieldParameter}
+
+object GreedyJsonExtractorLogic extends Described {
+
+  val fieldNameParameter = FieldNameParameterDescriptor(
+    ref = "fieldname",
+    info = ParameterInfo(
+      displayName = "fieldName.displayName",
+      description = "fieldName.description"
+    ),
+    mandatory = true
+  )
+
+  val prefixParameter = TextParameterDescriptor(
+    ref = "prefix",
+    info = ParameterInfo(
+      displayName = "prefix.displayName",
+      description = "prefix.description"
+    ),
+    mandatory = false,
+    defaultValue = "greedy"
+  )
+
+  val removeFieldParameter = BooleanParameterDescriptor(
+    ref = "removeField",
+    info = ParameterInfo(
+      displayName = TextRef("removeField.displayName"),
+      description = TextRef("removeField.description")
+    ),
+    defaultValue = false,
+    mandatory = false
+  )
+
+  override def describe = Descriptor(
+    ref = "7ffff5e5-4565-4cc5-85f4-ad1bb73bdd36",
+    describes = FilterDescriptor(
+      name = classOf[GreedyJsonExtractorLogic].getName,
+      displayName = TextRef("displayName"),
+      description = TextRef("description"),
+      categories = Seq(DECODING, JSON),
+      parameters = Seq(fieldNameParameter, prefixParameter, removeFieldParameter),
+      maturity = Experimental,
+      icon = Icon.fromClass(classOf[GreedyJsonExtractorLogic])
+    ),
+    localization = Localization.fromResourceBundle(
+      bundleName = "io.logbee.keyscore.pipeline.contrib.decoder.GreedyJsonExtractor",
+      Locale.ENGLISH, Locale.GERMAN
+    ) ++ CATEGORY_LOCALIZATION
+  )
+}
+
+class GreedyJsonExtractorLogic(parameters: LogicParameters, shape: FlowShape[Dataset, Dataset]) extends FilterLogic(parameters, shape) with StageLogging {
+
+  private var prefix = prefixParameter.defaultValue
+  private var fieldName = fieldNameParameter.defaultValue
+  private var removeField = removeFieldParameter.defaultValue
+
+  override def initialize(configuration: Configuration): Unit = {
+    configure(configuration)
+  }
+
+  override def configure(configuration: Configuration): Unit = {
+    fieldName = configuration.getValueOrDefault(fieldNameParameter, fieldName)
+    prefix = configuration.getValueOrDefault(prefixParameter, prefix)
+    prefix = if (prefix.nonEmpty) prefix else prefixParameter.defaultValue
+    removeField = configuration.getValueOrDefault(removeFieldParameter, removeField)
+  }
+
+  override def onPull(): Unit = {
+    pull(in)
+  }
+
+  override def onPush(): Unit = {
+    val dataset = grab(in)
+    val records = dataset.records.map(record => {
+
+      val sourceField = record.fields.find(fieldName == _.name)
+
+      if (sourceField.isDefined && sourceField.get.isTextField) {
+
+        val fields = JsonDecoderUtil.extract(sourceField.get.toTextField.value, prefix)
+
+        if(removeField) {
+          Record(record.fields.filter(_.name != fieldName) ++ fields)
+        }
+        else {
+          Record(record.fields ++ fields)
+        }
+      }
+      else {
+        record
+      }
+    })
+
+    push(out, Dataset(dataset.metadata, records))
+  }
+}
