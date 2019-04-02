98c2af15b9199574211dac29901ff7556c899721
Max K <ich@max-karthan.de>
2019-04-02 10:48:36 +0200

    KS:M => fixed issue with storybook running out of memory

--- keyscore-manager/.storybook/config.js
+++ keyscore-manager/.storybook/config.js
@@ -2,9 +2,11 @@ import { configure } from '@storybook/angular';
 import "../src/app/style/style.scss"
 
 // automatically import all files ending in *.stories.ts
-const req = require.context('../', true, /.stories.ts$/);
+const reqModules = require.context('../modules/', true, /.stories.ts$/);
+const reqMain = require.context('../src/stories/', true, /.stories.ts$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  reqModules.keys().forEach(filename => reqModules(filename));
+  reqMain.keys().forEach(filename => reqMain(filename));
 }
 
 configure(loadStories, module);
