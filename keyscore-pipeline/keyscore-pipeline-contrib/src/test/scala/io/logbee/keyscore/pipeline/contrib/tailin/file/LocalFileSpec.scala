e0ca99b220be981f2c0b33a4725368cebe77f6df
Zed Harper <zedharper@tutanota.com>
2019-04-11 09:37:22 +0200

    KS:P Tailin -> Simplified test code.

--- keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/tailin/file/LocalFileSpec.scala
+++ keyscore-pipeline/keyscore-pipeline-contrib/src/test/scala/io/logbee/keyscore/pipeline/contrib/tailin/file/LocalFileSpec.scala
@@ -8,92 +8,101 @@ import java.nio.ByteBuffer
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import java.nio.file.StandardOpenOption
+import java.nio.charset.StandardCharsets
 
 @RunWith(classOf[JUnitRunner])
 class LocalFileSpec extends SpecWithTempDir with Matchers {
   
+  val charset = StandardCharsets.UTF_8
   
-  def withLocalFile(testCode: (LocalFile, java.io.File) => Any) = {
-    val name = "localFile.txt"
+  
+  def withLocalFile(fileName: String, content: ByteBuffer, testCode: LocalFile => Any) = {
     
-    
-    val actualFile = watchDir.resolve(name).toFile
-    actualFile.createNewFile()
-    TestUtil.waitForFileToExist(actualFile)
-    
-    TestUtil.writeStringToFile(actualFile, "fileContent", StandardOpenOption.CREATE)
-    
+    val actualFile = TestUtil.createFile(watchDir, fileName, charset.decode(content).toString)
     
     val localFile = new LocalFile(actualFile)
     
-    testCode(localFile, actualFile)
+    try {
+      testCode(localFile)
+    }
+    finally {
+      if (actualFile != null) {
+        actualFile.delete()
+      }
+    }
   }
   
   
   
   "A LocalFile should" - {
-    "return its name" in withLocalFile {
-      (localFile, actualFile) =>
-        localFile.name shouldBe actualFile.getName
+    "return correct metadata" in {
+      
+      val fileName = "localFile.txt"
+      val content = charset.encode("fileContent")
+      
+      withLocalFile(fileName, content, {
+        localFile =>
+          localFile.name shouldBe fileName
+          
+          localFile.absolutePath shouldBe watchDir.resolve(fileName).toString
+          
+          val currentTime = System.currentTimeMillis
+          assert(localFile.lastModified >= currentTime - 3 * 1000)
+          assert(localFile.lastModified <= currentTime)
+          
+          localFile.length shouldBe content.limit
+      })
     }
     
     
-    "return its full path" in withLocalFile {
-      (localFile, actualFile) =>
-        localFile.absolutePath shouldBe watchDir.resolve(actualFile.getName).toString
+    "list its rotated files" in {
+      
+      val fileName = "localFile.txt"
+      
+      withLocalFile(fileName, charset.encode("fileContent"), {
+        localFile =>
+          val rotFile1 = TestUtil.createFile(watchDir, fileName + ".1", "fileContent1")
+          val rotFile2 = TestUtil.createFile(watchDir, fileName + ".2", "fileContent22")
+          
+          val rotationPattern = fileName + ".[1-5]"
+          
+          localFile.listRotatedFiles(rotationPattern) should contain allOf(rotFile1, rotFile2)
+      })
     }
     
     
-    "list its rotated files" in withLocalFile {
-      (localFile, actualFile) =>
-        val rotFile1 = TestUtil.createFile(watchDir, localFile.name + ".1", "fileContent1")
-        val rotFile2 = TestUtil.createFile(watchDir, localFile.name + ".2", "fileContent22")
-        
-        val rotationPattern = localFile.name + ".[1-5]"
-        
-        localFile.listRotatedFiles(rotationPattern) should contain allOf(rotFile1, rotFile2)
+    "read its content into a buffer" in {
+      
+      val content = charset.encode("fileContent")
+      
+      withLocalFile("localFile.txt", content, {
+        localFile =>
+          val buffer = ByteBuffer.allocate(content.limit)
+          
+          localFile.read(buffer, offset=0)
+          
+          buffer shouldBe content
+      })
     }
     
     
-    "return its lastModified" in withLocalFile {
-      (localFile, actualFile) =>
-        localFile.lastModified shouldBe actualFile.lastModified
-    }
-    
-    
-    "return its length" in withLocalFile {
-      (localFile, actualFile) =>
-        localFile.length shouldBe actualFile.length
-    }
-    
-    
-    "read its content into a buffer" in withLocalFile {
-      (localFile, actualFile) =>
-        val buffer = ByteBuffer.allocate(actualFile.length.asInstanceOf[Int])
-        
-        localFile.read(buffer, offset=0)
-        
-        val readString = new String(buffer.array)
-        val expectedString = scala.io.Source.fromFile(actualFile).mkString
-        
-        readString shouldBe expectedString
-    }
-    
-    
-    "read its content from an offset into a buffer" in withLocalFile {
-      (localFile, actualFile) =>
-        
-        val fileLength = actualFile.length.asInstanceOf[Int]
-        val offset = fileLength / 2
-        
-        val buffer = ByteBuffer.allocate(fileLength - offset)
-        
-        localFile.read(buffer, offset)
-        
-        val readString = new String(buffer.array)
-        val expectedString = scala.io.Source.fromFile(actualFile).mkString.substring(offset)
-        
-        readString shouldBe expectedString
+    "read its content from an offset into a buffer" in {
+      
+      val content = charset.encode("fileContent")
+      
+      withLocalFile("localFile.txt", content, {
+        localFile =>
+          val fileLength = content.limit
+          val offset = fileLength / 2
+          
+          val buffer = ByteBuffer.allocate(fileLength - offset)
+          
+          localFile.read(buffer, offset)
+          
+          buffer.array shouldBe content.array
+                                  .drop(offset)
+                                  .dropRight(content.capacity - content.limit) //the resulting array has 0s from the buffer's limit to the end, which we drop here
+      })
     }
   }
 }
