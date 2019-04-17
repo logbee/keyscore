733eb0493f52cb86521c87f2e7e39465186188c8
Elmar Schug <elmar.schug@jayware.org>
2019-04-17 15:37:29 +0200

    Added missing RunWith to DefaultIndexSpec_Records.

--- keyscore-pipeline/keyscore-pipeline-api/src/test/scala/io/logbee/keyscore/pipeline/api/util/DefaultIndexSpec_Records.scala
+++ keyscore-pipeline/keyscore-pipeline-api/src/test/scala/io/logbee/keyscore/pipeline/api/util/DefaultIndexSpec_Records.scala
@@ -5,8 +5,11 @@ import io.logbee.keyscore.model.util.ToOption.T2OptionT
 import io.logbee.keyscore.pipeline.api.util.Characteristic.whereAll
 import io.logbee.keyscore.pipeline.api.util.DefaultDataIndex.FIELD_PRESENT
 import io.logbee.keyscore.pipeline.api.util.DefaultDataIndex.Record.{byField, byPresentField, withField}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FreeSpec, Matchers}
 
+@RunWith(classOf[JUnitRunner])
 class DefaultIndexSpec_Records extends FreeSpec with Matchers {
 
   "A GroupingBookkeeper" - {
