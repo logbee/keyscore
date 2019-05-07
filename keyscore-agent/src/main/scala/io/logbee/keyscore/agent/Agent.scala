9f390c84f2a58808e04b4b257b984e49aa2796e9
Elmar Schug <elmar.schug@jayware.org>
2019-05-07 14:08:47 +0200

    Added the possibility to configure the name and id of an agent.

--- keyscore-agent/src/main/scala/io/logbee/keyscore/agent/Agent.scala
+++ keyscore-agent/src/main/scala/io/logbee/keyscore/agent/Agent.scala
@@ -16,8 +16,8 @@ import io.logbee.keyscore.commons.cluster.Topics.{AgentsTopic, ClusterTopic}
 import io.logbee.keyscore.commons.cluster._
 import io.logbee.keyscore.commons.extension.ExtensionLoader
 import io.logbee.keyscore.commons.extension.ExtensionLoader.LoadExtensions
+import io.logbee.keyscore.commons.util.StartUpWatch
 import io.logbee.keyscore.commons.util.StartUpWatch.StartUpComplete
-import io.logbee.keyscore.commons.util.{RandomNameGenerator, StartUpWatch}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
@@ -25,12 +25,15 @@ import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
 object Agent {
+
   case object Initialize
 
   private case object SendJoin
   private case object CheckJoin
 
   private case object ClusterAgentManagerDied
+
+  def apply(id: UUID, name: String): Props = Props(new Agent(id, name))
 }
 
 /**
@@ -42,13 +45,12 @@ object Agent {
   *   * [[io.logbee.keyscore.agent.pipeline.LocalPipelineManager]] <br>
   *   * [[io.logbee.keyscore.commons.extension.ExtensionLoader]] <br>
   */
-class Agent extends Actor with ActorLogging {
+class Agent(id: UUID, name: String) extends Actor with ActorLogging {
 
   private implicit val ec: ExecutionContext = context.dispatcher
   private implicit val timeout: Timeout = 30 seconds
 
   //Agent properties
-  private val agentName: String = new RandomNameGenerator("/agents.txt").nextName()
   private var joined: Boolean = false
 
   //Cluster
@@ -65,7 +67,7 @@ class Agent extends Actor with ActorLogging {
   private val extensionLoader = context.actorOf(Props[ExtensionLoader], "extension-loader")
 
   override def preStart(): Unit = {
-    log.info(s"The Agent ${agentName} has started.")
+    log.info(s"The Agent '$name' has started.")
     Cluster(context.system) registerOnMemberUp {
       scheduler.scheduleOnce(5 second) {
         self ! SendJoin
@@ -80,7 +82,7 @@ class Agent extends Actor with ActorLogging {
     mediator ! Publish(ClusterTopic, ActorLeave(Roles.AgentRole, self))
     mediator ! Unsubscribe(AgentsTopic, self)
     mediator ! Unsubscribe(ClusterTopic, self)
-    log.info(s"The Agent ${agentName} has stopped.")
+    log.info(s"The Agent '$name' has stopped.")
   }
 
   override def receive: Receive = {
@@ -97,7 +99,7 @@ class Agent extends Actor with ActorLogging {
       }
 
     case SendJoin =>
-      val agentJoin = AgentJoin(UUID.randomUUID(), agentName)
+      val agentJoin = AgentJoin(id, name)
       log.info(s"Trying to join cluster: $agentJoin")
       mediator ! Publish(AgentsTopic, agentJoin)
       scheduler.scheduleOnce(10 seconds) {
@@ -115,15 +117,15 @@ class Agent extends Actor with ActorLogging {
       (filterManager ? RequestDescriptors).mapTo[DescriptorsResponse].onComplete {
         case Success(message) =>
           mediator ! Publish(AgentsTopic, AgentCapabilities(message.descriptors))
-          log.info(s"Published ${agentName}'s capabilities to the topic agents.")
+          log.info(s"Published $name's capabilities to the topic agents.")
         case Failure(e) =>
-          log.error(e, s"Failed to publish ${agentName}'s capabilities!")
+          log.error(e, s"Failed to publish $name's capabilities!")
           context.stop(self)
       }
       context.watchWith(sender, ClusterAgentManagerDied)
 
     case AgentJoinFailure =>
-      log.error(s"Agent ${agentName}'s join failed")
+      log.error(s"Agent $name's join failed")
       context.stop(self)
 
     case ClusterAgentManagerDied =>
