fa9a2004969b6d8a9a061b8e5b9679b57bfb79de
Reimar Stier <github@reimarstier.de>
2019-07-01 16:50:20 +0200

    KS:M add prefix to environment variable KEYSCORE_BASE_URL

--- keyscore-manager/docker/docker-entrypoint.sh
+++ keyscore-manager/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ ! -z "$BASE_URL" ]; then
-    /bin/sed -e "s#\"base-url\".*#\"base-url\": \"$BASE_URL\"#g" -i /usr/share/nginx/html/application.conf
+if [ ! -z "$KEYSCORE_BASE_URL" ]; then
+    /bin/sed -e "s#\"base-url\".*#\"base-url\": \"$KEYSCORE_BASE_URL\"#g" -i /usr/share/nginx/html/application.conf
 fi
 
 exec /usr/sbin/nginx -g 'daemon off;'
