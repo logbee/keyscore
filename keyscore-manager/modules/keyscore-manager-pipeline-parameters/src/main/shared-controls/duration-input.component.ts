cf1970b6106e34143d4b2d903e223d906ef94af6
Max K <ich@max-karthan.de>
2019-07-11 16:24:39 +0200

    KS:M => added MatFormFieldControl for duration input

---
+++ keyscore-manager/modules/keyscore-manager-pipeline-parameters/src/main/shared-controls/duration-input.component.ts
@@ -0,0 +1,236 @@
+import {Component, ElementRef, EventEmitter, HostBinding, Input, OnDestroy, Output} from "@angular/core";
+import {FormBuilder, FormGroup, NgControl} from "@angular/forms";
+import {MatFormFieldControl} from "@angular/material";
+import {Subject} from "rxjs";
+import {FocusMonitor} from "@angular/cdk/a11y";
+import {coerceBooleanProperty} from "@angular/cdk/coercion";
+
+class Duration {
+    constructor(
+        readonly years: number,
+        readonly months: number,
+        readonly days: number,
+        readonly hours: number,
+        readonly minutes: number,
+        readonly seconds: number
+    ) {
+    }
+}
+
+@Component({
+    selector: 'ks-duration-input',
+    template: `
+        <div [formGroup]="parts" fxLayout="row">
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">Years</span>
+                <div fxLayout="row">
+                    <input type="number" min="0" formControlName="years" (change)="onChange()">
+                    <span fxLayoutAlign="center">:</span>
+                </div>
+            </div>
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">Months</span>
+                <div fxLayout="row">
+                    <input type="number" min="0" formControlName="months" (change)="onChange()">
+                    <span fxLayoutAlign="center">:</span>
+                </div>
+            </div>
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">Days</span>
+                <div fxLayout="row">
+                    <input type="number" min="0" formControlName="days" (change)="onChange()">
+                    <span fxLayoutAlign="center">:</span>
+                </div>
+            </div>
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">H</span>
+                <div fxLayout="row">
+                    <input type="number" min="0" formControlName="hours" (change)="onChange()">
+                    <span fxLayoutAlign="center">:</span>
+                </div>
+            </div>
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">Min</span>
+                <div fxLayout="row">
+                    <input type="number" min="0" formControlName="minutes" (change)="onChange()">
+                    <span fxLayoutAlign="center">:</span>
+                </div>
+            </div>
+            <div fxLayout="column" fxLayoutGap="5px">
+                <span fxFlexAlign="center" class="input-header">Sec</span>
+                <input type="number" min="0" formControlName="seconds" (change)="onChange()">
+            </div>
+        </div>
+    `,
+    styles: [`
+        div {
+            display: flex;
+        }
+
+        input {
+            border: none;
+            background: none;
+            padding: 0;
+            outline: none;
+            font: inherit;
+            text-align: end;
+            max-width: 4ch;
+            opacity: 0;
+            transition: opacity 200ms;
+        }
+
+        :host.floating input {
+            opacity: 1;
+        }
+
+        span {
+            opacity: 0;
+            transition: opacity 200ms;
+            display: block;
+        }
+
+        span.input-header {
+            font-size: 70%;
+            position:absolute;
+        }
+
+        :host.floating span.input-header {
+            position:relative;
+        }
+
+        :host.floating span {
+            opacity: 1;
+        }
+    `],
+    providers: [{provide: MatFormFieldControl, useExisting: DurationInputComponent}]
+})
+export class DurationInputComponent implements MatFormFieldControl<number>, OnDestroy {
+    static nextId = 0;
+
+    @HostBinding() id = `ks-duration-input-${DurationInputComponent.nextId++}`;
+    @HostBinding('attr.aria-describedby') describedBy = '';
+    parts: FormGroup;
+    stateChanges: Subject<void> = new Subject<void>();
+    ngControl: NgControl = null;
+    focused = false;
+    errorState = false;
+    constrolType = 'ks-duration-input';
+
+    @Input()
+    get value(): number {
+        const v = this.parts.value;
+        const durationInSeconds =
+            (v.years * 365 * 24 * 60 * 60) +
+            (v.months * 30 * 24 * 60 * 60) +
+            (v.days * 24 * 60 * 60) +
+            (v.hours * 60 * 60) +
+            (v.minutes * 60) + v.seconds;
+
+        return durationInSeconds;
+    }
+
+    set value(seconds: number) {
+        this.parts.setValue(this.secondsToDuration(seconds));
+        this.stateChanges.next();
+    }
+
+    @Input()
+    get placeholder() {
+        return this._placeholder;
+    }
+
+    set placeholder(plh) {
+        this._placeholder = plh;
+        this.stateChanges.next();
+    }
+
+    private _placeholder: string;
+
+    @Input()
+    get disabled(): boolean {
+        return this._disabled;
+    }
+
+    set disabled(value: boolean) {
+        this._disabled = coerceBooleanProperty(value);
+        this._disabled ? this.parts.disable() : this.parts.enable();
+        this.stateChanges.next();
+    }
+
+    private _disabled = false;
+
+    @Input()
+    get required() {
+        return this._required;
+    }
+
+    set required(req) {
+        this._required = coerceBooleanProperty(req);
+        this.stateChanges.next();
+    }
+
+    private _required = false;
+
+    get empty() {
+        let v = this.parts.value;
+        return !v.years && !v.months && !v.days && !v.hours && !v.minutes && !v.seconds;
+    }
+
+    @HostBinding('class.floating')
+    get shouldLabelFloat() {
+        return this.focused || !this.empty;
+    }
+
+    @Output() changed: EventEmitter<number> = new EventEmitter<number>();
+
+
+    constructor(fb: FormBuilder, private fm: FocusMonitor, private elRef: ElementRef<HTMLElement>) {
+        this.parts = fb.group({
+            'years': null,
+            'months': null,
+            'days': null,
+            'hours': null,
+            'minutes': null,
+            'seconds': null
+        });
+
+        fm.monitor(elRef.nativeElement, true).subscribe(origin => {
+            this.focused = !!origin;
+            this.stateChanges.next();
+        });
+    }
+
+    private onChange(){
+        this.changed.emit(this.value);
+    }
+
+    private secondsToDuration(seconds: number): Duration {
+        const years = seconds / (365 * 24 * 60 * 60);
+        seconds = seconds % (365 * 24 * 60 * 60);
+        const months = seconds / (30 * 24 * 60 * 60);
+        seconds = seconds % (30 * 24 * 60 * 60);
+        const days = seconds / (24 * 60 * 60);
+        seconds = seconds % (24 * 60 * 60);
+        const hours = seconds / (60 * 60);
+        seconds = seconds % (60 * 60);
+        const minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        return new Duration(years, months, days, hours, minutes, seconds);
+    }
+
+    onContainerClick(event: MouseEvent) {
+        if ((event.target as Element).tagName.toLowerCase() != 'input') {
+            this.elRef.nativeElement.querySelector('input').focus();
+        }
+    }
+
+    setDescribedByIds(ids: string[]) {
+        this.describedBy = ids.join(' ');
+    }
+
+    ngOnDestroy() {
+        this.stateChanges.complete();
+        this.fm.stopMonitoring(this.elRef.nativeElement);
+    }
+}
---
+++ keyscore-manager/modules/keyscore-manager-pipeline-parameters/src/stories/shared-controls.stories.ts
@@ -0,0 +1,35 @@
+import {moduleMetadata, storiesOf} from "@storybook/angular";
+import {action} from '@storybook/addon-actions';
+import {CommonModule} from "@angular/common";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from "keyscore-manager-material";
+import {DurationInputComponent} from "../main/shared-controls/duration-input.component";
+
+storiesOf('SharedControls/DurationInput', module)
+    .addDecorator(
+        moduleMetadata({
+            declarations: [
+                DurationInputComponent
+            ],
+            imports: [
+                CommonModule,
+                MaterialModule,
+                BrowserAnimationsModule,
+                ReactiveFormsModule
+            ],
+            providers: []
+        }))
+    .add("default", () => {
+        return {
+            template: `
+            <mat-form-field>
+                <ks-duration-input #input (changed)="changed($event)"></ks-duration-input>
+                <mat-label>Duration</mat-label>
+            </mat-form-field>
+          `,
+            props: {
+                changed: action('Value Change')
+            }
+        }
+    });
