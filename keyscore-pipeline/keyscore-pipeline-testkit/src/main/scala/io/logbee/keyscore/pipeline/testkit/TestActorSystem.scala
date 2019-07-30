938b56c076cdc3063b642a947ca8ca086314e772
Elmar Schug <elmar.schug@jayware.org>
2019-07-30 08:59:50 +0200

    Removed scalatest from the list of dependencies of the pipeline-testkit.

--- keyscore-pipeline/keyscore-pipeline-testkit/src/main/scala/io/logbee/keyscore/pipeline/testkit/TestActorSystem.scala
+++ keyscore-pipeline/keyscore-pipeline-testkit/src/main/scala/io/logbee/keyscore/pipeline/testkit/TestActorSystem.scala
@@ -2,21 +2,14 @@ package io.logbee.keyscore.pipeline.testkit
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import org.scalatest.{BeforeAndAfterAll, Suite}
 
-import scala.concurrent.Await.ready
 import scala.concurrent.ExecutionContextExecutor
-import scala.concurrent.duration._
 import scala.language.postfixOps
 
 
-trait TestActorSystem extends BeforeAndAfterAll  { this: Suite =>
+trait TestActorSystem  {
 
   implicit val system: ActorSystem = ActorSystem("test-system")
   implicit lazy val materializer: ActorMaterializer = ActorMaterializer()
   implicit lazy val executionContext: ExecutionContextExecutor = materializer.executionContext
-
-  override protected def afterAll(): Unit = {
-    ready(system.terminate(), 10 seconds)
-  }
 }
