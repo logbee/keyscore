c3c84e28aa245b83070d65911bec9621b11c3818
Max K <ich@max-karthan.de>
2019-09-26 20:09:24 +0200

    KS:M => feat(parameter-group) added parameter group models

---
+++ keyscore-manager/modules/keyscore-manager-models/src/main/parameters/group-parameter.model.ts
@@ -0,0 +1,48 @@
+import {Parameter, ParameterDescriptor} from "@keyscore-manager-models/src/main/parameters/parameter.model";
+import {ParameterRef} from "@keyscore-manager-models/src/main/common/Ref";
+
+export const JSONCLASS_GROUP_PARAM = "io.logbee.keyscore.model.configuration.GroupParameter";
+export const JSONCLASS_GROUP_DESCR = "io.logbee.keyscore.model.descriptor.GroupParameterDescriptor";
+
+export class ParameterGroupCondition {
+    constructor(
+        readonly parameter: ParameterRef
+    ) {
+    }
+}
+
+export class BooleanParameterCondition extends ParameterGroupCondition {
+    constructor(
+        readonly parameter: ParameterRef,
+        readonly negate: boolean
+    ) {
+        super(parameter);
+
+    }
+
+}
+
+export class ParameterGroupDescriptor extends ParameterDescriptor {
+    public readonly jsonClass = JSONCLASS_GROUP_DESCR;
+
+    constructor(
+        readonly ref: ParameterRef,
+        readonly displayName: string,
+        readonly description: string,
+        readonly condition: ParameterGroupCondition,
+        readonly parameters: ParameterDescriptor[]
+    ) {
+        super(ref, displayName, description);
+    }
+}
+
+export class ParameterGroup extends Parameter {
+    public readonly jsonClass = JSONCLASS_GROUP_PARAM;
+
+    constructor(
+        readonly ref: ParameterRef,
+        readonly value: Parameter[]
+    ) {
+        super(ref, value);
+    }
+}
